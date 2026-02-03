31a6e356ff16571a16ca2d21490b25e0b2a22813
aaron <aaronhowser1@gmail.com>
2026-02-03 14:09:19 -0600

    Add BedrockiumIngotItem and register it in ModItems

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/BedrockiumIngotItem.kt
@@ -0,0 +1,32 @@
+package dev.aaronhowser.mods.excessive_utilities.item
+
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.Entity
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+
+class BedrockiumIngotItem(properties: Properties) : Item(properties) {
+
+	override fun inventoryTick(
+		stack: ItemStack,
+		level: Level,
+		entity: Entity,
+		slotId: Int,
+		isSelected: Boolean
+	) {
+		if (level.isClientSide
+			|| entity !is LivingEntity
+			|| level.gameTime % 20L != 0L
+		) return
+
+		if (entity.hasEffect(MobEffects.DAMAGE_BOOST)) {
+			entity.removeEffect(MobEffects.MOVEMENT_SLOWDOWN)
+		} else {
+			entity.addEffect(MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 2))
+		}
+	}
+
+}
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/registry/ModItems.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/registry/ModItems.kt
@@ -2,11 +2,16 @@ package dev.aaronhowser.mods.excessive_utilities.registry
 
 import dev.aaronhowser.mods.aaron.registry.AaronItemRegistry
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.item.BedrockiumIngotItem
+import net.neoforged.neoforge.registries.DeferredItem
 import net.neoforged.neoforge.registries.DeferredRegister
 
 object ModItems : AaronItemRegistry() {
 
 	val ITEM_REGISTRY: DeferredRegister.Items = DeferredRegister.createItems(ExcessiveUtilities.MOD_ID)
 	override fun getItemRegistry(): DeferredRegister.Items = ITEM_REGISTRY
+
+	val BEDROCKIUM_INGOT: DeferredItem<BedrockiumIngotItem> =
+		register("bedrockium_ingot", ::BedrockiumIngotItem)
 
 }
