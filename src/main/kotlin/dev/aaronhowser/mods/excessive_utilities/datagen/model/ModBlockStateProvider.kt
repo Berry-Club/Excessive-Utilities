291237e13bf950a152b87b02290e4f400410d5a4
aaron <aaronhowser1@gmail.com>
2026-02-03 21:51:14 -0600

    Add blackout curtain model registration in ModBlockStateProvider

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/model/ModBlockStateProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/model/ModBlockStateProvider.kt
@@ -1,7 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.datagen.model
 
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.data.PackOutput
+import net.minecraft.world.level.block.Block
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider
 import net.neoforged.neoforge.common.data.ExistingFileHelper
 
@@ -11,7 +14,20 @@ class ModBlockStateProvider(
 ) : BlockStateProvider(output, ExcessiveUtilities.MOD_ID, existingFileHelper) {
 
 	override fun registerStatesAndModels() {
+		blackoutCurtain()
+	}
 
+	fun blackoutCurtain() {
+		val block = ModBlocks.BLACKOUT_CURTAIN.get()
+
+		models()
+			.withExistingParent("blackout_curtain_side", "block/glass_pane_side")
+			.texture("pane", "block/blackout_curtain")
+
+	}
+
+	private fun name(block: Block): String {
+		return BuiltInRegistries.BLOCK.getKey(block).path
 	}
 
 }
