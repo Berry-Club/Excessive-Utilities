5582c05839a029e2194319287b9c44cce58fe62f
aaron <aaronhowser1@gmail.com>
2026-02-04 00:22:31 -0600

    Add UUID handling and fake player initialization in Peaceful Table block entity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/PeacefulTableBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/PeacefulTableBlockEntity.kt
@@ -1,11 +1,15 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import com.mojang.authlib.GameProfile
 import com.mojang.datafixers.util.Either
+import dev.aaronhowser.mods.aaron.AaronExtensions.getUuidOrNull
 import dev.aaronhowser.mods.aaron.AaronExtensions.isServerSide
 import dev.aaronhowser.mods.aaron.AaronUtil
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.Difficulty
 import net.minecraft.world.DifficultyInstance
@@ -24,14 +28,39 @@ import net.minecraft.world.level.storage.loot.parameters.LootContextParams
 import net.neoforged.neoforge.capabilities.Capabilities
 import net.neoforged.neoforge.common.CommonHooks
 import net.neoforged.neoforge.common.extensions.IOwnedSpawner
+import net.neoforged.neoforge.common.util.FakePlayer
+import net.neoforged.neoforge.common.util.FakePlayerFactory
 import net.neoforged.neoforge.event.EventHooks
 import net.neoforged.neoforge.items.ItemHandlerHelper
+import java.lang.ref.WeakReference
+import java.util.*
 import kotlin.jvm.optionals.getOrNull
 
 class PeacefulTableBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState
 ) : BlockEntity(ModBlockEntityTypes.PEACEFUL_TABLE.get(), pos, blockState), IOwnedSpawner {
+
+	private var uuid: UUID? = null
+	private var fakePlayer: WeakReference<FakePlayer>? = null
+
+	private fun initFakePlayer() {
+		val level = level as? ServerLevel ?: return
+
+		if (this.uuid == null) {
+			this.uuid = UUID.randomUUID()
+			setChanged()
+		}
+
+		val gameProfile = GameProfile(this.uuid, "EU_PeacefulTable")
+		val fakePlayer = FakePlayerFactory.get(level, gameProfile)
+
+		fakePlayer.isSilent = true
+		fakePlayer.setOnGround(true)
+
+		this.fakePlayer = WeakReference(fakePlayer)
+		setChanged()
+	}
 
 	private fun tick() {
 		val level = this.level as? ServerLevel ?: return
@@ -72,7 +101,24 @@ class PeacefulTableBlockEntity(
 		return Either.left(this)
 	}
 
+	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.saveAdditional(tag, registries)
+
+		val uuid = this.uuid
+		if (uuid != null) {
+			tag.putUUID(UUID_NBT, uuid)
+		}
+	}
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+		this.uuid = tag.getUuidOrNull(UUID_NBT)
+	}
+
 	companion object {
+		const val UUID_NBT = "UUID"
+
 		fun tick(
 			level: Level,
 			blockPos: BlockPos,
