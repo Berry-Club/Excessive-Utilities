72f7d09532ae9a22a0203581eaebeac562e0ff35
aaron <aaronhowser1@gmail.com>
2026-02-05 02:54:42 -0600

    Implement GpSourceBlockEntity for grid power generation and update GpPanelBlockEntity to extend it

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/GpPanelBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/GpPanelBlockEntity.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
 import dev.aaronhowser.mods.aaron.AaronExtensions.isServerSide
+import dev.aaronhowser.mods.excessive_utilities.block.entity.base.GpSourceBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.config.ServerConfig
 import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerContribution
 import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerHandler
@@ -8,19 +9,15 @@ import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
-import java.util.*
 
 class GpPanelBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState,
 	val isDay: Boolean
-) : BlockEntity(if (isDay) ModBlockEntityTypes.SOLAR_PANEL.get() else ModBlockEntityTypes.LUNAR_PANEL.get(), pos, blockState) {
+) : GpSourceBlockEntity(if (isDay) ModBlockEntityTypes.SOLAR_PANEL.get() else ModBlockEntityTypes.LUNAR_PANEL.get(), pos, blockState) {
 
-	private var ownerUuid: UUID? = null
-
-	private val gpGeneration: GridPowerContribution =
+	override val gpGeneration: GridPowerContribution =
 		object : GridPowerContribution {
 			override fun getAmount(): Int {
 				val level = level ?: return 0
@@ -39,20 +36,6 @@ class GpPanelBlockEntity(
 
 			override fun isStillValid(): Boolean = !this@GpPanelBlockEntity.isRemoved
 		}
-
-	fun setOwner(uuid: UUID) {
-		ownerUuid = uuid
-	}
-
-	fun tick() {
-		val owner = ownerUuid ?: return
-		val level = level as? ServerLevel ?: return
-
-		if (gpGeneration.isStillValid() && gpGeneration.getAmount() > 0) {
-			val grid = GridPowerHandler.get(level).getGrid(owner)
-			grid.addProducer(gpGeneration)
-		}
-	}
 
 	companion object {
 		fun tick(
---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/base/GpSourceBlockEntity.kt
@@ -0,0 +1,35 @@
+package dev.aaronhowser.mods.excessive_utilities.block.entity.base
+
+import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerContribution
+import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerHandler
+import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+import java.util.*
+
+abstract class GpSourceBlockEntity(
+	type: BlockEntityType<*>,
+	pos: BlockPos,
+	blockState: BlockState
+) : BlockEntity(type, pos, blockState) {
+
+	protected var ownerUuid: UUID? = null
+	protected abstract val gpGeneration: GridPowerContribution
+
+	fun setOwner(uuid: UUID) {
+		ownerUuid = uuid
+	}
+
+	protected fun tick() {
+		val owner = ownerUuid ?: return
+		val level = level as? ServerLevel ?: return
+
+		if (gpGeneration.isStillValid() && gpGeneration.getAmount() > 0) {
+			val grid = GridPowerHandler.get(level).getGrid(owner)
+			grid.addProducer(gpGeneration)
+		}
+	}
+
+}
