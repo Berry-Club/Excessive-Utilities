77bab4c01df8531820367f5a0d4f56e2d3aeea31
aaron <aaronhowser1@gmail.com>
2026-02-05 03:03:32 -0600

    Refactor GpSourceBlock and GpPanelBlock to improve block entity ticker handling and constructor parameters

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/GpPanelBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/GpPanelBlock.kt
@@ -4,29 +4,18 @@ import dev.aaronhowser.mods.excessive_utilities.block.base.GpSourceBlock
 import dev.aaronhowser.mods.excessive_utilities.block.entity.GpPanelBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.block.entity.base.GpSourceBlockEntity
 import net.minecraft.core.BlockPos
-import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
-import net.minecraft.world.level.block.entity.BlockEntityTicker
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
 class GpPanelBlock(
 	val isDay: Boolean,
-	val beType: BlockEntityType<out GpSourceBlockEntity>
-) : GpSourceBlock(Properties.ofFullCopy(Blocks.DAYLIGHT_DETECTOR)) {
+	beType: BlockEntityType<out GpSourceBlockEntity>
+) : GpSourceBlock(beType, Properties.ofFullCopy(Blocks.DAYLIGHT_DETECTOR)) {
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return GpPanelBlockEntity(pos, state, isDay)
-	}
-
-	override fun <T : BlockEntity> getTicker(level: Level, state: BlockState, blockEntityType: BlockEntityType<T>): BlockEntityTicker<T>? {
-		return BaseEntityBlock.createTickerHelper(
-			blockEntityType,
-			beType,
-			GpSourceBlockEntity::tick
-		)
 	}
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/GpSourceBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/GpSourceBlock.kt
@@ -5,17 +5,32 @@ import net.minecraft.core.BlockPos
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.BaseEntityBlock
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 
-abstract class GpSourceBlock(properties: Properties) : Block(properties), EntityBlock {
+abstract class GpSourceBlock(
+	val beType: BlockEntityType<out GpSourceBlockEntity>,
+	properties: Properties,
+) : Block(properties), EntityBlock {
 
 	override fun setPlacedBy(level: Level, pos: BlockPos, state: BlockState, placer: LivingEntity?, stack: ItemStack) {
 		val blockEntity = level.getBlockEntity(pos)
 		if (blockEntity is GpSourceBlockEntity && placer != null) {
 			blockEntity.setOwner(placer.uuid)
 		}
+	}
+
+	override fun <T : BlockEntity> getTicker(level: Level, state: BlockState, blockEntityType: BlockEntityType<T>): BlockEntityTicker<T>? {
+		return BaseEntityBlock.createTickerHelper(
+			blockEntityType,
+			beType,
+			GpSourceBlockEntity::tick
+		)
 	}
 
 }
