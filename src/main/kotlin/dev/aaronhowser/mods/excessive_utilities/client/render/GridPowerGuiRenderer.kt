4c2bf445580ff7fea5bb883275bc4a1bf74ce0b1
aaron <aaronhowser1@gmail.com>
2026-02-05 02:34:48 -0600

    Add grid power display logic to GUI with overload indication

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/GridPowerGuiRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/GridPowerGuiRenderer.kt
@@ -4,8 +4,12 @@ import dev.aaronhowser.mods.aaron.AaronExtensions.isItem
 import dev.aaronhowser.mods.aaron.client.AaronClientUtil
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
 import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModItemTagsProvider
+import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.ClientGridPower
+import net.minecraft.ChatFormatting
 import net.minecraft.client.DeltaTracker
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 
 object GridPowerGuiRenderer {
@@ -13,9 +17,21 @@ object GridPowerGuiRenderer {
 	val LAYER_NAME: ResourceLocation = ExcessiveUtilities.modResource("grid_power_gui_layer")
 
 	fun renderGridPower(guiGraphics: GuiGraphics, deltaTracker: DeltaTracker) {
-		val localPlayer = AaronClientUtil.localPlayer ?: return
-		if (!localPlayer.isHolding { it.isItem(ModItemTagsProvider.RENDER_GP_WHILE_HOLDING) }) return
+		val player = AaronClientUtil.localPlayer ?: return
+		if (!player.isHolding { it.isItem(ModItemTagsProvider.RENDER_GP_WHILE_HOLDING) }) return
 
+		val component = Component.literal("${ClientGridPower.usage} / ${ClientGridPower.capacity} GP")
+		if (ClientGridPower.isOverloaded) {
+			component.withStyle(ChatFormatting.RED)
+		}
+
+		guiGraphics.drawString(
+			Minecraft.getInstance().font,
+			component,
+			guiGraphics.guiWidth() / 2 + 5,
+			guiGraphics.guiHeight() / 2 + 5,
+			0xFFFFFF
+		)
 	}
 
 }
