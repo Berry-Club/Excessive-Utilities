b1932183c2230e2b4ac68b7ca86c538846f0d2cb
aaron <aaronhowser1@gmail.com>
2026-02-05 04:45:36 -0600

    Add cranking functionality to Manual Mill block and entity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/ManualMillBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/ManualMillBlock.kt
@@ -5,10 +5,14 @@ import dev.aaronhowser.mods.excessive_utilities.block.entity.ManualMillBlockEnti
 import dev.aaronhowser.mods.excessive_utilities.block.entity.base.GpSourceBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.BlockHitResult
 
 class ManualMillBlock : GpSourceBlock(Properties.ofFullCopy(Blocks.STONE)) {
 
@@ -18,6 +22,16 @@ class ManualMillBlock : GpSourceBlock(Properties.ofFullCopy(Blocks.STONE)) {
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return ManualMillBlockEntity(pos, state)
+	}
+
+	override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult {
+		val blockEntity = level.getBlockEntity(pos)
+		if (blockEntity is ManualMillBlockEntity) {
+			blockEntity.startCranking(player)
+			return InteractionResult.sidedSuccess(level.isClientSide)
+		}
+
+		return InteractionResult.PASS
 	}
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/ManualMillBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/ManualMillBlockEntity.kt
@@ -1,9 +1,12 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.getPovResult
 import dev.aaronhowser.mods.excessive_utilities.block.entity.base.GpSourceBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.config.ServerConfig
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.block.state.BlockState
 
@@ -17,6 +20,24 @@ class ManualMillBlockEntity(
 
 	override fun getGp(): Int {
 		return ServerConfig.CONFIG.manualMillGenerationPerPlayer.get() * playersCranking.size
+	}
+
+	fun startCranking(player: Player) {
+		playersCranking.add(player)
+	}
+
+	override fun serverTick(level: ServerLevel) {
+		super.serverTick(level)
+
+		playersCranking.removeIf { player ->
+			val lookingAt = player.getPovResult().blockPos
+			lookingAt != this.blockPos
+		}
+
+		for (player in playersCranking) {
+			player.swing(InteractionHand.MAIN_HAND, true)
+		}
+
 	}
 
 }
