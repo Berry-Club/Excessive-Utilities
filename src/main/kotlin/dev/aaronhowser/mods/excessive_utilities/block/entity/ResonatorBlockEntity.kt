d70523a7a0ddc7eefbb246718aee7acc5b9af47b
aaron <aaronhowser1@gmail.com>
2026-02-05 05:54:12 -0600

    Implement recipe-based GP usage calculation in ResonatorBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/ResonatorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/ResonatorBlockEntity.kt
@@ -1,8 +1,11 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import dev.aaronhowser.mods.aaron.misc.ImprovedSimpleContainer
 import dev.aaronhowser.mods.excessive_utilities.block.entity.base.GpDrainBlockEntity
+import dev.aaronhowser.mods.excessive_utilities.recipe.resonator.ResonatorRecipe
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.state.BlockState
 
 class ResonatorBlockEntity(
@@ -10,6 +13,30 @@ class ResonatorBlockEntity(
 	blockState: BlockState
 ) : GpDrainBlockEntity(ModBlockEntityTypes.RESONATOR.get(), pos, blockState) {
 
-	override fun getGpUsage(): Int = 100
+	private val container = ImprovedSimpleContainer(this, CONTAINER_SIZE)
+
+	override fun getGpUsage(): Int = getRecipe()?.gpCost ?: 0
+
+	fun getRecipe(): ResonatorRecipe? {
+		val level = level ?: return null
+
+		val inputStack = container.getItem(INPUT_SLOT)
+		val recipe = ResonatorRecipe.getRecipe(level, inputStack) ?: return null
+
+		val stackInOutput = container.getItem(OUTPUT_SLOT)
+		if (stackInOutput.isEmpty) return recipe
+
+		val recipeOutput = recipe.getResultItem(level.registryAccess()).copy()
+		val outputCanFit = ItemStack.isSameItemSameComponents(stackInOutput, recipeOutput)
+				&& stackInOutput.count + recipeOutput.count <= stackInOutput.maxStackSize
+
+		return if (outputCanFit) recipe else null
+	}
+
+	companion object {
+		const val CONTAINER_SIZE = 2
+		const val INPUT_SLOT = 0
+		const val OUTPUT_SLOT = 1
+	}
 
 }
