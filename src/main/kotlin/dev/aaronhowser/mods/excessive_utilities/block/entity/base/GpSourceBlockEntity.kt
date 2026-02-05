a5dd92e42b7be35a61d35d6c068c5fad4bbf34b9
aaron <aaronhowser1@gmail.com>
2026-02-05 02:55:27 -0600

    Add owner UUID handling to GpSourceBlockEntity for persistence

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/base/GpSourceBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/base/GpSourceBlockEntity.kt
@@ -1,8 +1,11 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity.base
 
+import dev.aaronhowser.mods.aaron.AaronExtensions.getUuidOrNull
 import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerContribution
 import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerHandler
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
@@ -30,6 +33,25 @@ abstract class GpSourceBlockEntity(
 			val grid = GridPowerHandler.get(level).getGrid(owner)
 			grid.addProducer(gpGeneration)
 		}
+	}
+
+	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.saveAdditional(tag, registries)
+
+		val uuid = ownerUuid
+		if (uuid != null) {
+			tag.putUUID(OWNER_UUID_NBT, uuid)
+		}
+	}
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+		ownerUuid = tag.getUuidOrNull(OWNER_UUID_NBT)
+	}
+
+	companion object {
+		const val OWNER_UUID_NBT = "OwnerUUID"
 	}
 
 }
