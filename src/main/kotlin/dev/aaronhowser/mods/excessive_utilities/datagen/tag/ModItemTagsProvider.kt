236b5b97a9755d75823ae952b29c4b5ba114338d
aaron <aaronhowser1@gmail.com>
2026-02-05 02:18:26 -0600

    Add item tag for rendering grid power while holding specific items

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/tag/ModItemTagsProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/tag/ModItemTagsProvider.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.datagen.tag
 
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.data.tags.ItemTagsProvider
@@ -20,12 +21,18 @@ class ModItemTagsProvider(
 ) : ItemTagsProvider(pOutput, pLookupProvider, pBlockTags, ExcessiveUtilities.MOD_ID, existingFileHelper) {
 
 	override fun addTags(provider: HolderLookup.Provider) {
-
+		tag(RENDER_GP_WHILE_HOLDING)
+			.add(
+				ModItems.SPEED_UPGRADE.get(),
+				ModItems.POWER_MANAGER.get()
+			)
 	}
 
 	companion object {
 		private fun create(id: String): TagKey<Item> = ItemTags.create(ExcessiveUtilities.modResource(id))
 		private fun common(id: String): TagKey<Item> = ItemTags.create(ResourceLocation.fromNamespaceAndPath("c", id))
+
+		val RENDER_GP_WHILE_HOLDING = create("render_gp_while_holding")
 	}
 
 }
