0e9e81113c45ca2bf1b1dee9ee29130dbf4a0ee2
aaron <aaronhowser1@gmail.com>
2026-02-05 05:26:04 -0600

    Refactor Resonator recipe serializer registration to use method reference

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/registry/ModRecipeSerializers.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/registry/ModRecipeSerializers.kt
@@ -12,8 +12,8 @@ object ModRecipeSerializers {
 	val RECIPE_SERIALIZERS_REGISTRY: DeferredRegister<RecipeSerializer<*>> =
 		DeferredRegister.create(BuiltInRegistries.RECIPE_SERIALIZER, ExcessiveUtilities.MOD_ID)
 
-	val RESONATOR =
-		registerRecipeSerializer("resonator") { ResonatorRecipe.Serializer(::ResonatorRecipe) }
+	val RESONATOR: DeferredHolder<RecipeSerializer<*>, RecipeSerializer<*>> =
+		registerRecipeSerializer("resonator", ResonatorRecipe::Serializer)
 
 	private fun registerRecipeSerializer(
 		name: String,
