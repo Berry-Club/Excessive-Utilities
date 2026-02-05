86967625d628b6d3b516cdee50593374d79578ed
aaron <aaronhowser1@gmail.com>
2026-02-05 02:25:14 -0600

    Add client-side grid power management and update GUI rendering logic

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/GridPowerGuiRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/GridPowerGuiRenderer.kt
@@ -1,6 +1,9 @@
 package dev.aaronhowser.mods.excessive_utilities.client.render
 
+import dev.aaronhowser.mods.aaron.AaronExtensions.isItem
+import dev.aaronhowser.mods.aaron.client.AaronClientUtil
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModItemTagsProvider
 import net.minecraft.client.DeltaTracker
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.resources.ResourceLocation
@@ -10,6 +13,8 @@ object GridPowerGuiRenderer {
 	val LAYER_NAME: ResourceLocation = ExcessiveUtilities.modResource("grid_power_gui_layer")
 
 	fun renderGridPower(guiGraphics: GuiGraphics, deltaTracker: DeltaTracker) {
+		val localPlayer = AaronClientUtil.localPlayer ?: return
+		if (!localPlayer.isHolding { it.isItem(ModItemTagsProvider.RENDER_GP_WHILE_HOLDING) }) return
 
 	}
 
