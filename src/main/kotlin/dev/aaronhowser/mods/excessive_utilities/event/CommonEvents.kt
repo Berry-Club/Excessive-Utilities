ec89eb7833d3d0a6ff1849700ab31374e7e4eb89
aaron <aaronhowser1@gmail.com>
2026-02-05 01:41:36 -0600

    Add server tick event handler to update GridPowerHandler

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/event/CommonEvents.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/event/CommonEvents.kt
@@ -3,12 +3,14 @@ package dev.aaronhowser.mods.excessive_utilities.event
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
 import dev.aaronhowser.mods.excessive_utilities.block.entity.DrumBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.block.entity.MagnumTorchBlockEntity
+import dev.aaronhowser.mods.excessive_utilities.handler.grid_power.GridPowerHandler
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.capabilities.Capabilities
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent
 import net.neoforged.neoforge.event.entity.living.MobSpawnEvent
+import net.neoforged.neoforge.event.tick.ServerTickEvent
 
 @EventBusSubscriber(
 	modid = ExcessiveUtilities.MOD_ID
@@ -29,6 +31,11 @@ object CommonEvents {
 			DrumBlockEntity::getFluidCapability
 		)
 
+	}
+
+	@SubscribeEvent
+	fun afterServerTick(event: ServerTickEvent.Post) {
+		GridPowerHandler.get(event.server.overworld()).tick()
 	}
 
 }
