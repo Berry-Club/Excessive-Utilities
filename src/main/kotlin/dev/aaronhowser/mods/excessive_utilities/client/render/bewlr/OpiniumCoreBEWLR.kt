93acfea53680b772a4d211c2f38beb2637086b7d
aaron <aaronhowser1@gmail.com>
2026-02-07 00:26:58 -0600

    add rotation animation to OpiniumCoreBEWLR rendering

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/bewlr/OpiniumCoreBEWLR.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/bewlr/OpiniumCoreBEWLR.kt
@@ -1,11 +1,14 @@
 package dev.aaronhowser.mods.excessive_utilities.client.render.bewlr
 
 import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.math.Axis
+import dev.aaronhowser.mods.aaron.client.AaronClientUtil
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.ItemRenderer
+import net.minecraft.util.Mth
 import net.minecraft.world.item.ItemDisplayContext
 import net.minecraft.world.item.ItemStack
 import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions
@@ -44,6 +47,20 @@ class OpiniumCoreBEWLR : BlockEntityWithoutLevelRenderer(
 			poseStack.pushPose()
 
 			poseStack.translate(0.5, 0.5, 0.5)
+
+			val time = AaronClientUtil.localLevel?.gameTime ?: 0
+			val yRotPerTick = 4f
+			val yRot = Mth.wrapDegrees(time * yRotPerTick)
+			poseStack.mulPose(Axis.YP.rotationDegrees(yRot))
+
+			val xRotPerTick = 2f
+			val xRot = Mth.wrapDegrees(time * xRotPerTick)
+			poseStack.mulPose(Axis.XP.rotationDegrees(xRot))
+
+			val zRotPerTick = 1f
+			val zRot = Mth.wrapDegrees(time * zRotPerTick)
+			poseStack.mulPose(Axis.ZP.rotationDegrees(zRot))
+
 			poseStack.scale(0.75f, 0.75f, 0.75f)
 
 			ITEM_RENDERER.renderStatic(
