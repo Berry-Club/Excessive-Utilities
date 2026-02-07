1c730b2ba83f9cced62d7cc8474b8803162a29b0
aaron <aaronhowser1@gmail.com>
2026-02-07 04:15:14 -0600

    add owner UUID management to WirelessFeBatteryBlockEntity for battery tracking

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/WirelessFeBatteryBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/WirelessFeBatteryBlockEntity.kt
@@ -4,25 +4,40 @@ import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import java.util.UUID
 
 class WirelessFeBatteryBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState
 ) : BlockEntity(ModBlockEntityTypes.WIRELESS_FE_BATTERY.get(), pos, blockState) {
 
+	var ownerUuid: UUID? = null
+
 	override fun onLoad() {
 		super.onLoad()
 		val level = this.level
+		val owner = ownerUuid
+		if (level != null && owner != null) {
+			WirelessFeBatteryHandler.addBattery(level, owner, this)
+		}
 	}
 
 	override fun setRemoved() {
 		super.setRemoved()
 		val level = this.level
+		val owner = ownerUuid
+		if (level != null && owner != null) {
+			WirelessFeBatteryHandler.removeBattery(level, owner, this)
+		}
 	}
 
 	override fun clearRemoved() {
 		super.clearRemoved()
 		val level = this.level
+		val owner = ownerUuid
+		if (level != null && owner != null) {
+			WirelessFeBatteryHandler.addBattery(level, owner, this)
+		}
 	}
 
 }
