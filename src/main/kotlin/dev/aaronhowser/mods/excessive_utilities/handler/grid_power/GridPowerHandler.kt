34a36ca5d07c96d2c39fa274cad3a0dfce39a4ca
aaron <aaronhowser1@gmail.com>
2026-02-07 04:19:10 -0600

    stop saving data

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/grid_power/GridPowerHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/grid_power/GridPowerHandler.kt
@@ -1,10 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.handler.grid_power
 
 import net.minecraft.core.HolderLookup
-import net.minecraft.core.UUIDUtil
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.NbtUtils
-import net.minecraft.nbt.Tag
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.saveddata.SavedData
@@ -28,32 +25,14 @@ class GridPowerHandler : SavedData() {
 	}
 
 	override fun save(tag: CompoundTag, registries: HolderLookup.Provider): CompoundTag {
-		val listTag = tag.getList(GRID_IDS_NBT, Tag.TAG_INT_ARRAY.toInt())
-
-		for (gridId in grids.keys) {
-			val uuidTag = NbtUtils.createUUID(gridId)
-			listTag.add(uuidTag)
-		}
-
-		tag.put(GRID_IDS_NBT, listTag)
 		return tag
 	}
 
 	companion object {
-		const val GRID_IDS_NBT = "GridIDs"
 		const val SAVED_DATA_NAME = "eu_grid_power_handler"
 
 		private fun load(tag: CompoundTag, provider: HolderLookup.Provider): GridPowerHandler {
-			val handler = GridPowerHandler()
-
-			val listTag = tag.getList(GRID_IDS_NBT, Tag.TAG_INT_ARRAY.toInt())
-			for (i in listTag.indices) {
-				val intArray = listTag.getIntArray(i)
-				val uuid = UUIDUtil.uuidFromIntArray(intArray)
-				handler.grids[uuid] = GPGrid(uuid)
-			}
-
-			return handler
+			return GridPowerHandler()
 		}
 
 		fun get(level: ServerLevel): GridPowerHandler {
