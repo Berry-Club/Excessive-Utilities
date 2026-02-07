be5b99db91f9c75f9c7433fe15d4d9ff7d9cf446
aaron <aaronhowser1@gmail.com>
2026-02-07 06:50:15 -0600

    add DataDrivenGeneratorBlock class to support data-driven generator functionality

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/DataDrivenGeneratorBlock.kt
@@ -0,0 +1,37 @@
+package dev.aaronhowser.mods.excessive_utilities.block.base
+
+import dev.aaronhowser.mods.excessive_utilities.block.base.entity.CompressibleFeGeneratorBlockEntity
+import dev.aaronhowser.mods.excessive_utilities.block.entity.DataDrivenGeneratorBlockEntity
+import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.BaseEntityBlock
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.EntityBlock
+import net.minecraft.world.level.block.entity.BlockEntity
+import net.minecraft.world.level.block.entity.BlockEntityTicker
+import net.minecraft.world.level.block.entity.BlockEntityType
+import net.minecraft.world.level.block.state.BlockState
+
+abstract class DataDrivenGeneratorBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBlock {
+
+	abstract fun getBlockEntityType(): BlockEntityType<out DataDrivenGeneratorBlockEntity>
+
+	override fun setPlacedBy(level: Level, pos: BlockPos, state: BlockState, placer: LivingEntity?, stack: ItemStack) {
+		val blockEntity = level.getBlockEntity(pos)
+		if (blockEntity is DataDrivenGeneratorBlockEntity && placer != null) {
+			blockEntity.ownerUuid = placer.uuid
+		}
+	}
+
+	override fun <T : BlockEntity> getTicker(level: Level, state: BlockState, blockEntityType: BlockEntityType<T>): BlockEntityTicker<T>? {
+		return BaseEntityBlock.createTickerHelper(
+			blockEntityType,
+			getBlockEntityType(),
+			CompressibleFeGeneratorBlockEntity::tick
+		)
+	}
+
+}
