1974915646d89b919c2e2bdb074c81d6e8b4bac2
aaron <aaronhowser1@gmail.com>
2026-02-07 01:03:50 -0600

    refactor OpiniumCoreItem to use default tiers for contents component

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/OpiniumCoreItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/OpiniumCoreItem.kt
@@ -1,13 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
-import dev.aaronhowser.mods.aaron.misc.AaronExtensions.getDefaultInstance
 import dev.aaronhowser.mods.excessive_utilities.item.component.OpiniumCoreContentsComponent
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
-import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
 
 class OpiniumCoreItem(properties: Properties) : Item(properties) {
 
@@ -23,11 +20,7 @@ class OpiniumCoreItem(properties: Properties) : Item(properties) {
 			Properties()
 				.component(
 					ModDataComponents.OPINIUM_CORE_CONTENTS.get(),
-					OpiniumCoreContentsComponent(
-						Items.IRON_BLOCK.defaultInstance,
-						ModItems.RED_COAL.getDefaultInstance(),
-						Component.literal("Test")
-					)
+					OpiniumCoreContentsComponent.getDefaultTiers().first()
 				)
 		}
 	}
