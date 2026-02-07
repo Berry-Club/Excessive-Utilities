9cb653f34b1719d4c7a4b473f2117b03ffa443ca
aaron <aaronhowser1@gmail.com>
2026-02-07 05:58:04 -0600

    add burn time management and item container support to CompressibleFeGeneratorBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/base/CompressibleFeGeneratorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/base/CompressibleFeGeneratorBlockEntity.kt
@@ -1,10 +1,12 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity.base
 
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.getUuidOrNull
+import dev.aaronhowser.mods.aaron.misc.ImprovedSimpleContainer
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.ContainerHelper
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
@@ -21,6 +23,9 @@ abstract class CompressibleFeGeneratorBlockEntity(
 	abstract val generatorType: GeneratorType
 
 	var ownerUuid: UUID? = null
+	private var burnTimeRemaining: Int = 0
+
+	private val container: ImprovedSimpleContainer = ImprovedSimpleContainer(this, CONTAINER_SIZE)
 
 	protected open fun serverTick(level: ServerLevel) {
 
@@ -31,6 +36,9 @@ abstract class CompressibleFeGeneratorBlockEntity(
 	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.saveAdditional(tag, registries)
 
+		ContainerHelper.saveAllItems(tag, container.items, registries)
+		tag.putInt(BURN_TIME_REMAINING_NBT, burnTimeRemaining)
+
 		val uuid = ownerUuid
 		if (uuid != null) {
 			tag.putUUID(OWNER_UUID_NBT, uuid)
@@ -40,11 +48,17 @@ abstract class CompressibleFeGeneratorBlockEntity(
 	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.loadAdditional(tag, registries)
 
+		ContainerHelper.loadAllItems(tag, container.items, registries)
+		burnTimeRemaining = tag.getInt(BURN_TIME_REMAINING_NBT)
 		ownerUuid = tag.getUuidOrNull(OWNER_UUID_NBT)
 	}
 
 	companion object {
 		const val OWNER_UUID_NBT = "OwnerUUID"
+		const val BURN_TIME_REMAINING_NBT = "BurnTimeRemaining"
+
+		const val CONTAINER_SIZE = 1
+		const val INPUT_SLOT = 0
 
 		fun tick(
 			level: Level,
