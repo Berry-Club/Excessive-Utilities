1138cc994ebe6b3bb1d2c2c08d1158da326e871f
aaron <aaronhowser1@gmail.com>
2026-02-08 02:01:23 -0600

    feat: simplify node placement by utilizing FlatTransferNodeEntity in FlatTransferNodeItem

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/FlatTransferNodeItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/FlatTransferNodeItem.kt
@@ -1,12 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
-import dev.aaronhowser.mods.excessive_utilities.handler.flat_transfer_node.FlatTransferNode
-import dev.aaronhowser.mods.excessive_utilities.handler.flat_transfer_node.FlatTransferNodeHandler
+import dev.aaronhowser.mods.excessive_utilities.entity.FlatTransferNodeEntity
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.context.UseOnContext
-import java.util.*
 
 class FlatTransferNodeItem(properties: Properties) : Item(properties) {
 
@@ -17,16 +15,7 @@ class FlatTransferNodeItem(properties: Properties) : Item(properties) {
 			val direction = context.clickedFace
 			val pos = context.clickedPos
 
-			val handler = FlatTransferNodeHandler.get(level)
-
-			val node = FlatTransferNode(
-				id = UUID.randomUUID(),
-				onPos = pos,
-				facing = direction,
-				isItemNode = true,
-			)
-
-			handler.addNode(node)
+			FlatTransferNodeEntity.place(level, pos, direction, true)
 		}
 
 		return InteractionResult.SUCCESS
