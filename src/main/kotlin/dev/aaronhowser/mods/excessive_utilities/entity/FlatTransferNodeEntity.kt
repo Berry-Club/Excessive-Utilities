7171e31f9dc2735bdc8d9244fe863fb0ba9e503a
aaron <aaronhowser1@gmail.com>
2026-02-08 01:59:55 -0600

    feat: implement item transfer logic and node breaking functionality in FlatTransferNodeEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/entity/FlatTransferNodeEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/entity/FlatTransferNodeEntity.kt
@@ -1,5 +1,6 @@
 package dev.aaronhowser.mods.excessive_utilities.entity
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.tell
 import dev.aaronhowser.mods.excessive_utilities.registry.ModEntityTypes
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
@@ -13,6 +14,8 @@ import net.minecraft.world.entity.EntityType
 import net.minecraft.world.level.Level
 import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.Vec3
+import net.neoforged.neoforge.capabilities.Capabilities
+import net.neoforged.neoforge.items.ItemHandlerHelper
 
 class FlatTransferNodeEntity(entityType: EntityType<*>, level: Level) : Entity(entityType, level) {
 
@@ -23,6 +26,58 @@ class FlatTransferNodeEntity(entityType: EntityType<*>, level: Level) : Entity(e
 	var aiming: Direction
 		get() = entityData.get(AIMING_DATA)
 		private set(value) = entityData.set(AIMING_DATA, value)
+
+	override fun tick() {
+		super.tick()
+
+		val level = level()
+		if (level is ServerLevel) {
+			if (tryBreak(level)) {
+				discard()
+				return
+			}
+
+			if (isItemNode) {
+				transferItem(level)
+			}
+		}
+	}
+
+	private fun tryBreak(level: ServerLevel): Boolean {
+		val itemHandlerOn = level.getCapability(Capabilities.ItemHandler.BLOCK, blockPosition(), aiming)
+		val shouldBreak = itemHandlerOn == null
+
+		if (shouldBreak) {
+			val player = level.players().first()
+			player.tell("Breaking a node at $onPos")
+		}
+
+		return shouldBreak
+	}
+
+	private fun transferItem(level: ServerLevel) {
+		val pos = blockPosition()
+		val targetPos = pos.relative(aiming)
+
+		val source = level.getCapability(Capabilities.ItemHandler.BLOCK, pos, aiming) ?: return
+		val target = level.getCapability(Capabilities.ItemHandler.BLOCK, targetPos, aiming.opposite) ?: return
+
+		for (i in 0 until source.slots) {
+			val stackToMove = source.getStackInSlot(i)
+			if (stackToMove.isEmpty) continue
+
+			val copy = stackToMove.copyWithCount(1)
+
+			val simulatedResult = ItemHandlerHelper.insertItemStacked(target, copy, true)
+
+			if (simulatedResult.count != copy.count) {
+				val actualResult = ItemHandlerHelper.insertItemStacked(target, copy, false)
+				val amountInserted = copy.count - actualResult.count
+				stackToMove.shrink(amountInserted)
+				return
+			}
+		}
+	}
 
 	override fun defineSynchedData(builder: SynchedEntityData.Builder) {
 		builder.define(IS_ITEM_NODE_DATA, true)
