76b9dfe34ba02cb8ec28d863ab12d68b062f7909
aaron <aaronhowser1@gmail.com>
2026-02-08 00:09:50 -0600

    feat: implement chunk-based node management in FlatTransferNodesHandler

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/flat_transfer_node/FlatTransferNodesHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/handler/flat_transfer_node/FlatTransferNodesHandler.kt
@@ -1,15 +1,40 @@
 package dev.aaronhowser.mods.excessive_utilities.handler.flat_transfer_node
 
-import dev.aaronhowser.mods.aaron.misc.ObservableMutableSet
-import dev.aaronhowser.mods.aaron.misc.observableMutableSetOf
 import net.minecraft.core.HolderLookup
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.level.ChunkPos
 import net.minecraft.world.level.saveddata.SavedData
 
 class FlatTransferNodesHandler : SavedData() {
 
-	private val nodes: ObservableMutableSet<FlatTransferNode> = observableMutableSetOf { setDirty() }
+	private val nodes: MutableSet<FlatTransferNode> = mutableSetOf()
+	private val nodesPerChunk: MutableMap<ChunkPos, MutableSet<FlatTransferNode>> = mutableMapOf()
+
+	fun getNodesInChunk(chunkPos: ChunkPos): Set<FlatTransferNode> {
+		return nodesPerChunk.get(chunkPos)?.toSet() ?: emptySet()
+	}
+
+	fun addNode(node: FlatTransferNode) {
+		nodes.add(node)
+
+		val chunkPos = node.chunkPos
+		val chunkNodes = nodesPerChunk.getOrPut(chunkPos) { mutableSetOf() }
+		chunkNodes.add(node)
+	}
+
+	fun removeNode(node: FlatTransferNode) {
+		nodes.remove(node)
+
+		val chunkPos = node.chunkPos
+		val chunkNodes = nodesPerChunk[chunkPos]
+		if (chunkNodes != null) {
+			chunkNodes.remove(node)
+			if (chunkNodes.isEmpty()) {
+				nodesPerChunk.remove(chunkPos)
+			}
+		}
+	}
 
 	fun tick(level: ServerLevel) {
 		for (node in nodes) {
