987e385bbc49f9e46dff6c4ca9c660955896c771
aaron <aaronhowser1@gmail.com>
2026-02-08 00:47:17 -0600

    register pacetk

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/packet/ModPacketHandler.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/packet/ModPacketHandler.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.packet
 
 import dev.aaronhowser.mods.aaron.packet.AaronPacketRegistrar
+import dev.aaronhowser.mods.excessive_utilities.packet.server_to_client.UpdateChunkFlatTransferNodesPacket
 import dev.aaronhowser.mods.excessive_utilities.packet.server_to_client.UpdateGridPowerPacket
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
 
@@ -13,6 +14,12 @@ object ModPacketHandler : AaronPacketRegistrar {
 			registrar,
 			UpdateGridPowerPacket.TYPE,
 			UpdateGridPowerPacket.STREAM_CODEC
+		)
+
+		toClient(
+			registrar,
+			UpdateChunkFlatTransferNodesPacket.TYPE,
+			UpdateChunkFlatTransferNodesPacket.STREAM_CODEC
 		)
 
 	}
