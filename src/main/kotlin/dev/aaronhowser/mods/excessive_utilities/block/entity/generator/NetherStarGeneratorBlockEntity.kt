79c30462ef6c2f07dc327ff4e8a8da65056635c0
aaron <aaronhowser1@gmail.com>
2026-02-10 10:52:07 -0600

    feat: add particle effect to Nether Star generator when active

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/NetherStarGeneratorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/NetherStarGeneratorBlockEntity.kt
@@ -4,6 +4,8 @@ import dev.aaronhowser.mods.excessive_utilities.block.GeneratorBlock
 import dev.aaronhowser.mods.excessive_utilities.block.base.DataDrivenGeneratorType
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.core.particles.ColorParticleOption
+import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
@@ -35,11 +37,19 @@ class NetherStarGeneratorBlockEntity(
 	}
 
 	override fun clientTick(level: Level) {
-		val isActive = blockState.getValue(GeneratorBlock.Companion.LIT)
+		val isActive = blockState.getValue(GeneratorBlock.LIT)
+		if (!isActive) return
 
-		if (isActive) {
-
-		}
+		val particle = ColorParticleOption.create(ParticleTypes.ENTITY_EFFECT, -1)
+		level.addAlwaysVisibleParticle(
+			particle,
+			blockPos.x.toDouble(),
+			blockPos.y + 1.0,
+			blockPos.z.toDouble(),
+			0.0,
+			0.0,
+			0.0
+		)
 	}
 
 }
