f65826b64831d0dc4d85da49854cb2bd9986cc19
aaron <aaronhowser1@gmail.com>
2026-02-10 04:26:26 -0600

    sync

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/CreativeHarvestBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/CreativeHarvestBlockEntity.kt
@@ -6,6 +6,9 @@ import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NbtUtils
+import net.minecraft.network.protocol.Packet
+import net.minecraft.network.protocol.game.ClientGamePacketListener
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
@@ -36,6 +39,10 @@ class CreativeHarvestBlockEntity(
 		val readBLockState = NbtUtils.readBlockState(registries.lookupOrThrow(Registries.BLOCK), blockStateTag)
 		mimicBlockState = readBLockState
 	}
+
+	// Syncs with client
+	override fun getUpdateTag(pRegistries: HolderLookup.Provider): CompoundTag = saveWithoutMetadata(pRegistries)
+	override fun getUpdatePacket(): Packet<ClientGamePacketListener> = ClientboundBlockEntityDataPacket.create(this)
 
 	companion object {
 		const val MIMIC_BLOCK_NBT = "MimicBlock"
