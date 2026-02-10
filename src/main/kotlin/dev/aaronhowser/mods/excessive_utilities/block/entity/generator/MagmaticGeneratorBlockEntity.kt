0fc740f99065d925b1c786cd88f2be3476953c7f
aaron <aaronhowser1@gmail.com>
2026-02-10 12:43:09 -0600

    feat: implement fluid fuel validation and burning logic in MagmaticGeneratorBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/MagmaticGeneratorBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/MagmaticGeneratorBlockEntity.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity.generator
 
 import dev.aaronhowser.mods.excessive_utilities.block.base.entity.GeneratorBlockEntity
+import dev.aaronhowser.mods.excessive_utilities.datamap.MagmaticGeneratorFuel
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
 import net.minecraft.core.HolderLookup
@@ -8,6 +9,7 @@ import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.block.state.BlockState
 import net.neoforged.neoforge.fluids.FluidStack
+import net.neoforged.neoforge.fluids.capability.IFluidHandler
 import net.neoforged.neoforge.fluids.capability.templates.FluidTank
 
 class MagmaticGeneratorBlockEntity(
@@ -18,9 +20,30 @@ class MagmaticGeneratorBlockEntity(
 	val tank: FluidTank =
 		object : FluidTank(1_000_000) {
 			override fun isFluidValid(stack: FluidStack): Boolean {
-				return super.isFluidValid(stack)
+				val fuelMap = MagmaticGeneratorFuel.MAP
+				val fluidFuel = stack.fluid.builtInRegistryHolder().getData(fuelMap)
+
+				return fluidFuel != null
 			}
 		}
+
+	override fun tryStartBurning(level: ServerLevel): Boolean {
+		if (burnTimeRemaining > 0) return false
+
+		val fluidInTank = tank.fluid
+		if (fluidInTank.isEmpty) return false
+
+		val fuelMap = MagmaticGeneratorFuel.MAP
+		val fluidFuel = fluidInTank.fluid.builtInRegistryHolder().getData(fuelMap) ?: return false
+
+		fePerTick = fluidFuel.fePerTick
+		burnTimeRemaining = fluidFuel.ticksPerMb
+
+		tank.drain(1, IFluidHandler.FluidAction.EXECUTE)
+		setChanged()
+
+		return true
+	}
 
 	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.saveAdditional(tag, registries)
@@ -30,10 +53,6 @@ class MagmaticGeneratorBlockEntity(
 	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
 		super.loadAdditional(tag, registries)
 		tank.readFromNBT(registries, tag)
-	}
-
-	override fun tryStartBurning(level: ServerLevel): Boolean {
-		TODO("Not yet implemented")
 	}
 
 }
