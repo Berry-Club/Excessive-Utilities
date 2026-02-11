eef5461a2d6d49531722a2f1283ca424c5663604
aaron <aaronhowser1@gmail.com>
2026-02-11 14:19:00 -0600

    feat: refactor BagOfHoldingMenu to use MenuWithInventory and add validation for item holding

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/bag_of_holding/BagOfHoldingMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/bag_of_holding/BagOfHoldingMenu.kt
@@ -1,8 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.menu.bag_of_holding
 
-import dev.aaronhowser.mods.excessive_utilities.menu.HeldItemContainerMenu
+import dev.aaronhowser.mods.aaron.menu.MenuWithInventory
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isItem
 import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
 import dev.aaronhowser.mods.excessive_utilities.registry.ModMenuTypes
+import net.minecraft.world.Container
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.Slot
@@ -10,26 +12,20 @@ import net.minecraft.world.item.ItemStack
 
 class BagOfHoldingMenu(
 	containerId: Int,
-	playerInventory: Inventory
-) : HeldItemContainerMenu(
-	ModItems.BAG_OF_HOLDING,
-	ModMenuTypes.BAG_OF_HOLDING.get(),
-	containerId,
-	playerInventory
-) {
+	playerInventory: Inventory,
+	val bagContainer: Container
+) : MenuWithInventory(ModMenuTypes.BAG_OF_HOLDING.get(), containerId, playerInventory) {
 
 	init {
 		addSlots()
 		addPlayerInventorySlots(51)
 	}
 
-	override val containerSlots: Int = 9 * 6
-
 	override fun addSlots() {
 		for (row in 0 until 6) {
 			for (col in 0 until 9) {
 				val index = col + row * 9
-				val slot = Slot(fakeContainer, index, 8 + col * 18, 8 + row * 18)
+				val slot = Slot(bagContainer, index, 8 + col * 18, 8 + row * 18)
 				addSlot(slot)
 			}
 		}
@@ -37,6 +33,10 @@ class BagOfHoldingMenu(
 
 	override fun quickMoveStack(player: Player, index: Int): ItemStack {
 		return ItemStack.EMPTY
+	}
+
+	override fun stillValid(player: Player): Boolean {
+		return player.isHolding { it.isItem(ModItems.BAG_OF_HOLDING) }
 	}
 
 }
