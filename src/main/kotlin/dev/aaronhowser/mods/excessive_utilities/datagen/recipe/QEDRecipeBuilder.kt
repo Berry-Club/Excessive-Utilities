79bb1710a4b046ae6b13e4d637873f3412f94ef3
aaron <aaronhowser1@gmail.com>
2026-02-11 17:10:48 -0600

    feat: implement QED recipe builder for crafting recipes

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/recipe/QEDRecipeBuilder.kt
@@ -0,0 +1,70 @@
+package dev.aaronhowser.mods.excessive_utilities.datagen.recipe
+
+import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.recipe.QEDRecipe
+import net.minecraft.advancements.AdvancementRequirements
+import net.minecraft.advancements.AdvancementRewards
+import net.minecraft.advancements.Criterion
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger
+import net.minecraft.data.recipes.RecipeBuilder
+import net.minecraft.data.recipes.RecipeOutput
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.Ingredient
+import net.minecraft.world.item.crafting.ShapedRecipePattern
+
+class QEDRecipeBuilder(
+	val result: ItemStack,
+	val count: Int = 1
+) : RecipeBuilder {
+
+	private val criteria: MutableMap<String, Criterion<*>> = mutableMapOf()
+	private val keys: MutableMap<Char, Ingredient> = mutableMapOf()
+	private val rows: MutableList<String> = mutableListOf()
+
+	fun define(key: Char, ingredient: Ingredient): QEDRecipeBuilder {
+		keys[key] = ingredient
+		return this
+	}
+
+	fun pattern(vararg rows: String): QEDRecipeBuilder {
+		this.rows.addAll(rows)
+		return this
+	}
+
+	override fun unlockedBy(name: String, criterion: Criterion<*>): RecipeBuilder {
+		criteria[name] = criterion
+		return this
+	}
+
+	override fun group(p0: String?): RecipeBuilder {
+		error("Unsupported")
+	}
+
+	override fun getResult(): Item = result.item
+
+	override fun save(recipeOutput: RecipeOutput, id: ResourceLocation) {
+		val idString = StringBuilder()
+
+		idString
+			.append("enchanter/")
+			.append(id.path)
+
+		val id = ExcessiveUtilities.modResource(idString.toString())
+
+		val advancement = recipeOutput.advancement()
+			.addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+			.rewards(AdvancementRewards.Builder.recipe(id))
+			.requirements(AdvancementRequirements.Strategy.OR)
+
+		for (criterion in criteria) {
+			advancement.addCriterion(criterion.key, criterion.value)
+		}
+
+		val pattern = ShapedRecipePattern.of(keys, rows)
+
+		val recipe = QEDRecipe(pattern, result)
+		recipeOutput.accept(id, recipe, advancement.build(id.withPrefix("recipes/")))
+	}
+}
