25a744e5fb393992d99ad1bcaea6c70814adb0ce
aaron <aaronhowser1@gmail.com>
2026-02-11 14:00:54 -0600

    feat: implement BagOfHoldingItem class with menu integration and display name

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/BagOfHoldingItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/BagOfHoldingItem.kt
@@ -1,10 +1,33 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
+import dev.aaronhowser.mods.excessive_utilities.menu.bag_of_holding.BagOfHoldingMenu
 import net.minecraft.core.component.DataComponents
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.MenuProvider
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.component.ItemContainerContents
+import net.minecraft.world.level.Level
 
-class BagOfHoldingItem(properties: Properties) : Item(properties) {
+class BagOfHoldingItem(properties: Properties) : Item(properties), MenuProvider {
+
+	override fun use(level: Level, player: Player, usedHand: InteractionHand): InteractionResultHolder<ItemStack?> {
+		player.openMenu(this)
+
+		val usedStack = player.getItemInHand(usedHand)
+		return InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
+	}
+
+	override fun getDisplayName(): Component = defaultInstance.hoverName
+
+	override fun createMenu(containerId: Int, playerInventory: Inventory, player: Player): AbstractContainerMenu {
+		return BagOfHoldingMenu(containerId, playerInventory)
+	}
 
 	companion object {
 		val DEFAULT_PROPERTIES: Properties =
