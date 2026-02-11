a5abbe5c4cfd2a7e1a771e061f5f2d96ac95980a
aaron <aaronhowser1@gmail.com>
2026-02-11 13:49:38 -0600

    feat: enhance Bag of Holding menu with item container functionality

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/HeldItemContainerMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/HeldItemContainerMenu.kt
@@ -2,12 +2,15 @@ package dev.aaronhowser.mods.excessive_utilities.menu
 
 import dev.aaronhowser.mods.aaron.menu.MenuWithInventory
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isItem
+import net.minecraft.core.component.DataComponents
 import net.minecraft.world.InteractionHand
+import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.MenuType
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.component.ItemContainerContents
 import net.neoforged.neoforge.registries.DeferredItem
 
 abstract class HeldItemContainerMenu(
@@ -24,6 +27,8 @@ abstract class HeldItemContainerMenu(
 		playerInventory: Inventory
 	) : this(deferredItem.get(), menuType, containerId, playerInventory)
 
+	abstract val containerSlots: Int
+
 	open fun getHeldItemStack(): ItemStack {
 		return if (playerInventory.player.mainHandItem.isItem(item)) {
 			playerInventory.player.mainHandItem
@@ -31,6 +36,25 @@ abstract class HeldItemContainerMenu(
 			playerInventory.player.offhandItem
 		}
 	}
+
+	fun getItemContainer(): ItemContainerContents? {
+		return getHeldItemStack().get(DataComponents.CONTAINER)
+	}
+
+	val fakeContainer: SimpleContainer =
+		object : SimpleContainer(containerSlots) {
+
+			override fun getItem(index: Int): ItemStack = getItemContainer()?.getStackInSlot(index) ?: ItemStack.EMPTY
+
+			override fun removeItem(index: Int, count: Int): ItemStack {
+				val container = getItemContainer() ?: return ItemStack.EMPTY
+
+				if (index !in 0 until container.slots) return ItemStack.EMPTY
+				val stack = container.getStackInSlot(index).copy()
+
+			}
+
+		}
 
 	protected val hand: InteractionHand =
 		if (playerInventory
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/bag_of_holding/BagOfHoldingMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/bag_of_holding/BagOfHoldingMenu.kt
@@ -3,9 +3,11 @@ package dev.aaronhowser.mods.excessive_utilities.menu.bag_of_holding
 import dev.aaronhowser.mods.excessive_utilities.menu.HeldItemContainerMenu
 import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
 import dev.aaronhowser.mods.excessive_utilities.registry.ModMenuTypes
+import net.minecraft.core.component.DataComponents
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.component.ItemContainerContents
 
 class BagOfHoldingMenu(
 	containerId: Int,
