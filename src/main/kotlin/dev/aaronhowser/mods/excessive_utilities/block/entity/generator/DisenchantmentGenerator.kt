59d75ba6b98bdbd6808d219d3dd3c2d957a91aa9
aaron <aaronhowser1@gmail.com>
2026-02-11 04:27:41 -0600

    feat: update DisenchantmentGenerator to refine power calculation and restrict input to enchanted books

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/DisenchantmentGenerator.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/generator/DisenchantmentGenerator.kt
@@ -1,21 +1,20 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity.generator
 
-import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isHolder
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isItem
 import dev.aaronhowser.mods.excessive_utilities.block.base.GeneratorContainer
 import dev.aaronhowser.mods.excessive_utilities.block.base.entity.GeneratorBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
-import net.minecraft.core.Holder
-import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.Mth
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.alchemy.Potions
+import net.minecraft.world.item.Items
 import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
-import kotlin.jvm.optionals.getOrNull
+import kotlin.math.min
+import kotlin.math.sqrt
 
 class DisenchantmentGenerator(
 	pos: BlockPos,
@@ -26,22 +25,12 @@ class DisenchantmentGenerator(
 		if (burnTimeRemaining > 0) return false
 
 		val inputStack = container.getItem(GeneratorContainer.INPUT_SLOT)
-		if (inputStack.isEmpty) return false
+		if (!inputStack.isItem(Items.ENCHANTED_BOOK)) return false
 
-		val potion = inputStack.get(DataComponents.POTION_CONTENTS)?.potion?.getOrNull() ?: return false
+		val totalPower = getPowerFromEnchantment(level, inputStack)
+		if (totalPower <= 0) return false
 
-		if (potion.isHolder(Potions.WATER)) {
-			burnTimeRemaining = 10
-			fePerTick = 1
-		} else {
-			val totalPower = getPowerFromEnchantment(level, inputStack)
-			if (totalPower <= 0) return false
 
-			val duration = 200
-
-			fePerTick = Mth.ceil(totalPower / duration.toDouble())
-			burnTimeRemaining = duration
-		}
 
 		inputStack.shrink(1)
 		setChanged()
@@ -56,15 +45,22 @@ class DisenchantmentGenerator(
 			var totalPower = 0
 
 			for ((enchantment, enchantLevel) in enchantments.entrySet()) {
-				totalPower += getPowerFromEnchantment(level, enchantment, enchantLevel)
+				totalPower += getPowerFromEnchantment(enchantment.value(), enchantLevel)
 			}
 
 			return totalPower
 		}
 
-		fun getPowerFromEnchantment(level: Level, enchantment: Holder<Enchantment>, enchantLevel: Int): Int {
+		fun getPowerFromEnchantment(enchantment: Enchantment, level: Int): Int {
+			val minLevel = enchantment.minLevel
+			val maxLevel = enchantment.maxLevel
+			val weight = enchantment.weight
 
+			val a = sqrt(min(level + 1, maxLevel) / maxLevel.toDouble())
+			val b = (maxLevel * maxLevel) * (level + 1)
+			val c = minLevel / sqrt(weight.toDouble())
 
+			return Mth.ceil(a * b * c) * 400
 		}
 	}
 
