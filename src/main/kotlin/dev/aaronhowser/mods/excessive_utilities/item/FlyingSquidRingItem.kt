170279b76662e584805a3a79ad5c72dedae23be2
aaron <aaronhowser1@gmail.com>
2026-02-13 09:10:53 -0600

    feat: decrement charge on Flying Squid Ring usage

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/FlyingSquidRingItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/FlyingSquidRingItem.kt
@@ -31,6 +31,8 @@ class FlyingSquidRingItem(properties: Properties) : Item(properties) {
 			val charge = stack.getOrDefault(ModDataComponents.CHARGE.get(), 0)
 			if (charge <= 0) return
 
+			stack.set(ModDataComponents.CHARGE.get(),  charge - 1)
+
 			val movement = entity.deltaMovement
 			val dy = movement.y
 			val gravity = entity.gravity
