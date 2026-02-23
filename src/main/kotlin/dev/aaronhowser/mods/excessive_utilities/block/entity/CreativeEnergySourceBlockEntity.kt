18dccaaf32d0051cb36cd1d1e0960f9f0fae52f8
aaron <aaronhowser1@gmail.com>
2026-02-22 22:25:02 -0600

    add energy storage implementation to CreativeEnergySourceBlockEntity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/CreativeEnergySourceBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/CreativeEnergySourceBlockEntity.kt
@@ -6,11 +6,22 @@ import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.neoforged.neoforge.energy.IEnergyStorage
 
 class CreativeEnergySourceBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState
 ) : BlockEntity(ModBlockEntityTypes.CREATIVE_ENERGY_SOURCE.get(), pos, blockState) {
+
+	val energyStorage: IEnergyStorage =
+		object : IEnergyStorage {
+			override fun receiveEnergy(toReceive: Int, simulate: Boolean): Int = 0
+			override fun extractEnergy(toExtract: Int, simulate: Boolean): Int = toExtract
+			override fun getEnergyStored(): Int = Int.MAX_VALUE
+			override fun getMaxEnergyStored(): Int = Int.MAX_VALUE
+			override fun canExtract(): Boolean = true
+			override fun canReceive(): Boolean = false
+		}
 
 	private fun serverTick(level: ServerLevel) {
 
