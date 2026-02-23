83fb1d88fe78489c37a8e14f2e8cef593ed74251
aaron <aaronhowser1@gmail.com>
2026-02-23 01:44:15 -0600

    feat: enhance SoulFragmentItem tooltips with Soul Debt and health modifier information

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/language/ModTooltipLang.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/language/ModTooltipLang.kt
@@ -3,10 +3,16 @@ package dev.aaronhowser.mods.excessive_utilities.datagen.language
 object ModTooltipLang {
 
 	const val SOUL_OF_A_FORGOTTEN_DEITY = "tooltip.excessive_utilities.soul_of_a_forgotten_deity"
+	const val SOUL_DEBT = "tooltip.excessive_utilities.soul_debt"
+	const val SOUL_SURPLUS = "tooltip.excessive_utilities.soul_surplus"
+	const val SOUL_HEALTH_MODIFIER = "tooltip.excessive_utilities.soul_health_modifier"
 
 	fun add(provider: ModLanguageProvider) {
 		provider.apply {
 			add(SOUL_OF_A_FORGOTTEN_DEITY, "Soul of a Forgotten Deity")
+			add(SOUL_DEBT, "Soul Debt: %d")
+			add(SOUL_SURPLUS, "Soul Surplus: %d")
+			add(SOUL_HEALTH_MODIFIER, "Current Health Modifier: %s")
 		}
 	}
 
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/SoulFragmentItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/SoulFragmentItem.kt
@@ -1,12 +1,20 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
+import dev.aaronhowser.mods.aaron.client.AaronClientUtil
 import dev.aaronhowser.mods.excessive_utilities.attachment.SoulDebt
+import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModLanguageProvider.Companion.toComponent
+import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModTooltipLang
+import dev.aaronhowser.mods.excessive_utilities.registry.ModAttachmentTypes
+import net.minecraft.ChatFormatting
 import net.minecraft.core.component.DataComponents
+import net.minecraft.network.chat.Component
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 
 class SoulFragmentItem(properties: Properties) : Item(properties) {
@@ -20,6 +28,45 @@ class SoulFragmentItem(properties: Properties) : Item(properties) {
 		}
 
 		return InteractionResultHolder.sidedSuccess(stack, level.isClientSide)
+	}
+
+	override fun appendHoverText(
+		stack: ItemStack,
+		context: TooltipContext,
+		tooltipComponents: MutableList<Component>,
+		tooltipFlag: TooltipFlag
+	) {
+		val player = AaronClientUtil.localPlayer ?: return
+		val netSoulFragments = player.getData(ModAttachmentTypes.SOUL_DEBT).netSoulFragments
+
+		if (netSoulFragments > 0) {
+			tooltipComponents.add(
+				ModTooltipLang.SOUL_SURPLUS
+					.toComponent(netSoulFragments)
+					.withStyle(ChatFormatting.LIGHT_PURPLE)
+			)
+		} else {
+			tooltipComponents.add(
+				ModTooltipLang.SOUL_DEBT
+					.toComponent(-netSoulFragments)
+					.withStyle(ChatFormatting.RED)
+			)
+		}
+
+		val attributeModifier = player
+			.getAttribute(Attributes.MAX_HEALTH)
+			?.getModifier(SoulDebt.ATTRIBUTE_MODIFIER_NAME)
+
+		if (attributeModifier != null) {
+			val amount = attributeModifier.amount
+			if (amount != 0.0) {
+				tooltipComponents.add(
+					ModTooltipLang.SOUL_HEALTH_MODIFIER
+						.toComponent(if (amount > 0) "+$amount" else "$amount")
+						.withStyle(ChatFormatting.GRAY)
+				)
+			}
+		}
 	}
 
 	companion object {
