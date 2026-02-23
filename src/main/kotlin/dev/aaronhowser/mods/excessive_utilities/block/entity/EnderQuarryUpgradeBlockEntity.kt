bdf6fe7b819dd62afb9f2f58981832cba639ed6a
aaron <aaronhowser1@gmail.com>
2026-02-22 18:41:07 -0600

    feat: add quarry position handling to Ender Quarry upgrade block entity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/EnderQuarryUpgradeBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/EnderQuarryUpgradeBlockEntity.kt
@@ -3,6 +3,8 @@ package dev.aaronhowser.mods.excessive_utilities.block.entity
 import dev.aaronhowser.mods.excessive_utilities.block.base.EnderQuarryUpgrade
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 
@@ -17,5 +19,36 @@ class EnderQuarryUpgradeBlockEntity(
 			field = value
 			setChanged()
 		}
+
+	var quarryPos: BlockPos? = null
+		set(value) {
+			if (field == value) return
+			field = value
+			setChanged()
+		}
+
+	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.saveAdditional(tag, registries)
+
+		tag.putString(UPGRADE_TAG, upgrade.id)
+		val qPos = quarryPos
+		if (qPos != null) {
+			tag.putLong(QUARRY_POS_TAG, qPos.asLong())
+		}
+	}
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+		upgrade = EnderQuarryUpgrade.valueOf(tag.getString(UPGRADE_TAG))
+		if (tag.contains(QUARRY_POS_TAG)) {
+			quarryPos = BlockPos.of(tag.getLong(QUARRY_POS_TAG))
+		}
+	}
+
+	companion object {
+		const val UPGRADE_TAG = "Upgrade"
+		const val QUARRY_POS_TAG = "QuarryPos"
+	}
 
 }
