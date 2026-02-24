077c767d6c024574ec153f9ae5a3299fa1097e45
aaron <aaronhowser1@gmail.com>
2026-02-24 01:12:50 -0600

    feat: implement use method for Magical Boomerang item to spawn entity on server

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/MagicalBoomerangItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/MagicalBoomerangItem.kt
@@ -1,8 +1,31 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isServerSide
+import dev.aaronhowser.mods.excessive_utilities.entity.MagicalBoomerangEntity
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
 
 class MagicalBoomerangItem(properties: Properties) : Item(properties) {
+
+	override fun use(
+		level: Level,
+		player: Player,
+		usedHand: InteractionHand
+	): InteractionResultHolder<ItemStack?> {
+		val stack = player.getItemInHand(usedHand)
+
+		if (level.isServerSide) {
+			val boomerang = MagicalBoomerangEntity(level, player)
+			boomerang.shootFromRotation(player, player.xRot, player.yRot, 0f, 1.5f, 0f)
+			level.addFreshEntity(boomerang)
+		}
+
+		return InteractionResultHolder.sidedSuccess(stack, level.isClientSide)
+	}
 
 	companion object {
 		val DEFAULT_PROPERTIES: Properties = Properties().stacksTo(1)
