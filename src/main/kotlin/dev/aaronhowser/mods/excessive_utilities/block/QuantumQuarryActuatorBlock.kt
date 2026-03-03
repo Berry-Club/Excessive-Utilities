2b07161baff54f06951401e821c98abfa33c09da
aaron <aaronhowser1@gmail.com>
2026-03-03 07:02:08 -0600

    feat: add facing property and placement logic to QuantumQuarryActuatorBlock

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/QuantumQuarryActuatorBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/QuantumQuarryActuatorBlock.kt
@@ -5,6 +5,7 @@ import dev.aaronhowser.mods.excessive_utilities.block.entity.QuantumQuarryActuat
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
@@ -12,8 +13,39 @@ import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.DirectionProperty
 
 class QuantumQuarryActuatorBlock : Block(Properties.ofFullCopy(Blocks.OBSIDIAN)), EntityBlock {
+
+	init {
+		registerDefaultState(
+			stateDefinition.any()
+				.setValue(FACING, Direction.UP)
+		)
+	}
+
+	override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+		builder.add(FACING)
+	}
+
+	override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
+		val pos = context.clickedPos
+		val level = context.level
+
+		val quarryDirection = Direction.entries.find { direction ->
+			val adjacentPos = pos.relative(direction)
+			val adjacentState = level.getBlockState(adjacentPos)
+			adjacentState.isBlock(ModBlocks.QUANTUM_QUARRY)
+		}
+
+		return if (quarryDirection != null) {
+			defaultBlockState().setValue(FACING, quarryDirection.opposite)
+		} else {
+			null
+		}
+	}
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return QuantumQuarryActuatorBlockEntity(pos, state)
@@ -42,6 +74,10 @@ class QuantumQuarryActuatorBlock : Block(Properties.ofFullCopy(Blocks.OBSIDIAN))
 		} else {
 			Blocks.AIR.defaultBlockState()
 		}
+	}
+
+	companion object {
+		val FACING: DirectionProperty = BlockStateProperties.FACING
 	}
 
 }
