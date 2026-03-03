5ebb24f6913b2bd51bcf76a6f0fc761ffc33f31d
aaron <aaronhowser1@gmail.com>
2026-03-03 14:29:07 -0600

    add createLayer method for AngelRingWingsModel to define wing geometry

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/layer/AngelRingWingsModel.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/layer/AngelRingWingsModel.kt
@@ -4,8 +4,14 @@ import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.blaze3d.vertex.VertexConsumer
 import net.minecraft.client.model.EntityModel
 import net.minecraft.client.model.geom.ModelPart
+import net.minecraft.client.model.geom.PartPose
+import net.minecraft.client.model.geom.builders.CubeDeformation
+import net.minecraft.client.model.geom.builders.CubeListBuilder
+import net.minecraft.client.model.geom.builders.LayerDefinition
+import net.minecraft.client.model.geom.builders.MeshDefinition
 import net.minecraft.client.player.AbstractClientPlayer
 import net.minecraft.client.renderer.RenderType
+import net.minecraft.util.Mth
 
 class AngelRingWingsModel : EntityModel<AbstractClientPlayer> {
 
@@ -37,4 +43,39 @@ class AngelRingWingsModel : EntityModel<AbstractClientPlayer> {
 	) {
 
 	}
+
+	companion object {
+		fun createLayer(): LayerDefinition {
+			val meshDefinition = MeshDefinition()
+			val partDefinition = meshDefinition.root
+			val cubeDeformation = CubeDeformation(1f)
+
+			partDefinition.addOrReplaceChild(
+				"left_wing",
+				CubeListBuilder.create()
+					.texOffs(22, 0)
+					.addBox(
+						-10f, 0f, 0f,
+						10f, 20f, 2f,
+						cubeDeformation
+					),
+				PartPose.offsetAndRotation(5f, 0f, 0f, Mth.PI / 12f, 0f, -Mth.PI / 12f)
+			)
+
+			partDefinition.addOrReplaceChild(
+				"right_wing",
+				CubeListBuilder.create()
+					.texOffs(22, 0)
+					.addBox(
+						0f, 0f, 0f,
+						10f, 20f, 2f,
+						cubeDeformation
+					),
+				PartPose.offsetAndRotation(-5f, 0f, 0f, Mth.PI / 12f, 0f, Mth.PI / 12f)
+			)
+
+			return LayerDefinition.create(meshDefinition, 64, 32)
+		}
+	}
+
 }
