d1aa2493dafc43e2a55fd9d69231719fd2001dd5
aaron <aaronhowser1@gmail.com>
2026-03-03 08:12:33 -0600

    feat: implement DropOfEvilItem functionality to replace blocks with cursed earth

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/DropOfEvilItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/DropOfEvilItem.kt
@@ -1,7 +1,46 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
+import dev.aaronhowser.mods.aaron.block_walker.BlockWalker
+import dev.aaronhowser.mods.aaron.block_walker.WalkType
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.defaultBlockState
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isBlock
+import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModBlockTagsProvider
+import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
+import net.minecraft.world.InteractionResult
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.block.Block
 
 class DropOfEvilItem(properties: Properties) : Item(properties) {
+
+	override fun useOn(context: UseOnContext): InteractionResult {
+		val level = context.level
+		val pos = context.clickedPos
+		val blockState = level.getBlockState(pos)
+		if (!blockState.isBlock(ModBlockTagsProvider.CURSED_EARTH_REPLACEABLE)) return InteractionResult.PASS
+
+		val blockWalker = BlockWalker(
+			level = level,
+			walkType = WalkType.ALL_SURROUNDING,
+			startPos = pos,
+			filter = { level, pos, state ->
+				state.isBlock(ModBlockTagsProvider.CURSED_EARTH_REPLACEABLE) && level.isEmptyBlock(pos.above())
+			},
+			maxDistance = 5,
+			maxTotalBlocks = 1000,
+			onFinished = { walkedBlocks ->
+				for (block in walkedBlocks) {
+					val (pos, state) = block.block
+
+					if (state.isBlock(ModBlockTagsProvider.CURSED_EARTH_REPLACEABLE)) {
+						level.setBlock(pos, ModBlocks.CURSED_EARTH.defaultBlockState(), Block.UPDATE_ALL)
+					}
+				}
+			})
+
+		blockWalker.start(3)
+
+		return InteractionResult.SUCCESS
+	}
 
 }
