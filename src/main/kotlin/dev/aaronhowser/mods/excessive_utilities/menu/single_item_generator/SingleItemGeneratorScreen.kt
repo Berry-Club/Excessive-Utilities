22fec89afe49931f81b0740df691c26bc72c73af
aaron <aaronhowser1@gmail.com>
2026-03-04 11:26:43 -0600

    add EnergyBar to SingleItemGeneratorScreen for energy display

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/single_item_generator/SingleItemGeneratorScreen.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/single_item_generator/SingleItemGeneratorScreen.kt
@@ -3,8 +3,7 @@ package dev.aaronhowser.mods.excessive_utilities.menu.single_item_generator
 import dev.aaronhowser.mods.aaron.menu.BaseScreen
 import dev.aaronhowser.mods.aaron.menu.textures.ScreenBackground
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
-import dev.aaronhowser.mods.excessive_utilities.menu.resonator.ResonatorMenu
-import dev.aaronhowser.mods.excessive_utilities.menu.single_slot.SingleSlotMenu
+import dev.aaronhowser.mods.excessive_utilities.menu.components.EnergyBar
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 
@@ -15,6 +14,22 @@ class SingleItemGeneratorScreen(
 ) : BaseScreen<SingleItemGeneratorMenu>(menu, playerInventory, title) {
 
 	override val background: ScreenBackground = BACKGROUND
+
+	private lateinit var energyBar: EnergyBar
+
+	override fun baseInit() {
+		super.baseInit()
+
+		energyBar = EnergyBar(
+			x = leftPos + 151,
+			y = topPos + 15,
+			maxGetter = { menu.getMaxEnergy() },
+			currentGetter = { menu.getCurrentEnergy() },
+			font = font
+		)
+
+		addRenderableWidget(energyBar)
+	}
 
 	companion object {
 		val BACKGROUND = ScreenBackground(ExcessiveUtilities.modResource("textures/gui/single_item_generator.png"), 176, 172)
