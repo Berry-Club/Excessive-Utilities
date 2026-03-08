3c5f31e601245e66d7042990c29bd93cebcd7acc
aaron <aaronhowser1@gmail.com>
2026-03-08 07:49:24 -0500

    implement output slot and add slots method in QED menu

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/qed/QedMenu.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/menu/qed/QedMenu.kt
@@ -1,12 +1,14 @@
 package dev.aaronhowser.mods.excessive_utilities.menu.qed
 
 import dev.aaronhowser.mods.aaron.menu.MenuWithInventory
+import dev.aaronhowser.mods.aaron.menu.components.OutputSlot
 import dev.aaronhowser.mods.excessive_utilities.block.entity.QedBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModMenuTypes
 import net.minecraft.world.Container
 import net.minecraft.world.SimpleContainer
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.Slot
 import net.minecraft.world.item.ItemStack
 
 class QedMenu(
@@ -21,6 +23,26 @@ class QedMenu(
 				playerInventory,
 				SimpleContainer(QedBlockEntity.CONTAINER_SIZE),
 			)
+
+	init {
+		checkContainerSize(qedContainer, QedBlockEntity.CONTAINER_SIZE)
+
+		addSlots()
+		addPlayerInventorySlots(90)
+	}
+
+	override fun addSlots() {
+		val outputSlot = OutputSlot(qedContainer, QedBlockEntity.OUTPUT_SLOT, 124, 35)
+		addSlot(outputSlot)
+
+		for (i in 0 until 3) for (j in 0 until 3) {
+			val index = j * 3 + i
+			val x = 30 + i * 18
+			val y = 17 + j * 18
+			val slot = Slot(qedContainer, index, x, y)
+			addSlot(slot)
+		}
+	}
 
 	override fun quickMoveStack(player: Player, index: Int): ItemStack {
 		return ItemStack.EMPTY
