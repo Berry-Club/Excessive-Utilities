809ac06c5ea0cf3012e72466a74e8321861daa34
aaron <aaronhowser1@gmail.com>
2026-03-08 07:19:26 -0500

    implement container functionality in QED block entity with item extraction

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/QedBlockEntity.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/entity/QedBlockEntity.kt
@@ -1,8 +1,16 @@
 package dev.aaronhowser.mods.excessive_utilities.block.entity
 
+import dev.aaronhowser.mods.aaron.container.ExtractOnlyInvWrapper
+import dev.aaronhowser.mods.aaron.container.ImprovedSimpleContainer
+import dev.aaronhowser.mods.excessive_utilities.block.base.ContainerContainer
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.core.HolderLookup
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.Container
+import net.minecraft.world.ContainerHelper
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -10,13 +18,44 @@ import net.minecraft.world.level.block.state.BlockState
 class QedBlockEntity(
 	pos: BlockPos,
 	blockState: BlockState
-) : BlockEntity(ModBlockEntityTypes.QED.get(), pos, blockState) {
+) : BlockEntity(ModBlockEntityTypes.QED.get(), pos, blockState), ContainerContainer {
+
+	private val container: ImprovedSimpleContainer = ImprovedSimpleContainer(this, CONTAINER_SIZE)
+	override fun getContainers(): List<Container> {
+		return listOf(container)
+	}
+
+	private val itemHandler: ExtractOnlyInvWrapper =
+		object : ExtractOnlyInvWrapper(container) {
+			override fun extractItem(slot: Int, amount: Int, simulate: Boolean): ItemStack {
+				return if (slot == OUTPUT_SLOT) {
+					super.extractItem(slot, amount, simulate)
+				} else {
+					ItemStack.EMPTY
+				}
+			}
+		}
 
 	private fun serverTick(level1: ServerLevel) {
 
 	}
 
+	override fun saveAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.saveAdditional(tag, registries)
+
+		ContainerHelper.saveAllItems(tag, container.items, registries)
+	}
+
+	override fun loadAdditional(tag: CompoundTag, registries: HolderLookup.Provider) {
+		super.loadAdditional(tag, registries)
+
+		ContainerHelper.loadAllItems(tag, container.items, registries)
+	}
+
 	companion object {
+		const val OUTPUT_SLOT = 9
+		const val CONTAINER_SIZE = 10
+
 		fun tick(
 			level: Level,
 			blockPos: BlockPos,
