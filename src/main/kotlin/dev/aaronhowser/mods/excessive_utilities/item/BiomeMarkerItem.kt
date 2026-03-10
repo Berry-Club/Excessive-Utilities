5c17fa2934cd88ad71bb01c619fd0c723c8a354c
aaron <aaronhowser1@gmail.com>
2026-03-09 19:07:37 -0500

    add hover text for BiomeMarkerItem displaying biome information

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/BiomeMarkerItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/BiomeMarkerItem.kt
@@ -4,14 +4,39 @@ import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isHolder
 import dev.aaronhowser.mods.excessive_utilities.datagen.tag.ModBiomeTagsProvider
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
 import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
+import net.minecraft.ChatFormatting
+import net.minecraft.client.resources.language.I18n
 import net.minecraft.core.Holder
 import net.minecraft.core.HolderLookup
 import net.minecraft.core.registries.Registries
+import net.minecraft.network.chat.Component
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.biome.Biome
 
 class BiomeMarkerItem(properties: Properties) : Item(properties) {
+
+	override fun appendHoverText(
+		stack: ItemStack,
+		context: TooltipContext,
+		tooltipComponents: MutableList<Component>,
+		tooltipFlag: TooltipFlag
+	) {
+		val biomeHolder = stack.get(ModDataComponents.BIOME) ?: return
+		val biomeKey = biomeHolder.key ?: return
+
+		val probableTranslationKey = "biome.${biomeKey.location().namespace}.${biomeKey.location().path}"
+		val hasTranslation = I18n.exists(probableTranslationKey)
+
+		val component = if (hasTranslation) {
+			Component.translatable(probableTranslationKey)
+		} else {
+			Component.literal(biomeKey.location().toString())
+		}.withStyle(ChatFormatting.GRAY)
+
+		tooltipComponents.add(component)
+	}
 
 	companion object {
 
