ae1af12ea4a744dbc9da2f59fdc537a38caebb63
aaron <aaronhowser1@gmail.com>
2026-03-11 17:11:51 -0500

    feat: add TransferNodePing class for managing ping positions in transfer nodes

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/base/TransferNodePing.kt
@@ -0,0 +1,15 @@
+package dev.aaronhowser.mods.excessive_utilities.block.base
+
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+
+// https://github.com/Leclowndu93150/Extra-Utilities/blob/master/src/main/java/com/leclowndu93150/extrautils2/blockentity/transfer/TransferNodePing.java
+class TransferNodePing(
+	private val homePos: BlockPos,
+	private val homePlacedOnDirection: Direction
+) {
+
+	var currentPingPos: BlockPos = homePos
+		private set
+
+}
