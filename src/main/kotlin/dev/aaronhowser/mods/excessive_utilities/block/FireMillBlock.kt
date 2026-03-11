eb7df718c3fd4665d6fa2b164842a7f3aecb4abf
aaron <aaronhowser1@gmail.com>
2026-03-11 10:48:23 -0500

    feat: add shape definition for FireMill block

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/FireMillBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/FireMillBlock.kt
@@ -5,10 +5,14 @@ import dev.aaronhowser.mods.excessive_utilities.block.base.entity.GpSourceBlockE
 import dev.aaronhowser.mods.excessive_utilities.block.entity.FireMillBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 
 class FireMillBlock : GpSourceBlock(Properties.ofFullCopy(Blocks.STONE)) {
 
@@ -18,6 +22,21 @@ class FireMillBlock : GpSourceBlock(Properties.ofFullCopy(Blocks.STONE)) {
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return FireMillBlockEntity(pos, state)
+	}
+
+	override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+		return SHAPE
+	}
+
+	companion object {
+		val SHAPE: VoxelShape =
+			Shapes.or(
+				box(0.0, 0.0, 0.0, 1.0, 16.0, 16.0),
+				box(0.0, 0.0, 0.0, 16.0, 16.0, 1.0),
+				box(15.0, 0.0, 0.0, 16.0, 16.0, 16.0),
+				box(0.0, 0.0, 15.0, 16.0, 16.0, 16.0),
+				box(1.0, 7.0, 1.0, 15.0, 7.1, 15.0)
+			)
 	}
 
 }
