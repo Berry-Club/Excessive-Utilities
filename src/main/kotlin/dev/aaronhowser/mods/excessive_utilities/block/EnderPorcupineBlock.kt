04861aeb0842e0005b00f72288710dce722b92d2
aaron <aaronhowser1@gmail.com>
2026-03-17 19:27:06 -0500

    mmmmmmm

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnderPorcupineBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnderPorcupineBlock.kt
@@ -1,7 +1,10 @@
 package dev.aaronhowser.mods.excessive_utilities.block
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isServerSide
 import dev.aaronhowser.mods.excessive_utilities.block_entity.EnderPorcupineBlockEntity
 import net.minecraft.core.BlockPos
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
@@ -12,6 +15,27 @@ class EnderPorcupineBlock : Block(Properties.ofFullCopy(Blocks.STONE)), EntityBl
 
 	override fun newBlockEntity(pos: BlockPos, state: BlockState): BlockEntity {
 		return EnderPorcupineBlockEntity(pos, state)
+	}
+
+	override fun playerWillDestroy(
+		level: Level,
+		pos: BlockPos,
+		state: BlockState,
+		player: Player
+	): BlockState {
+		if (level.isServerSide) {
+			val blockEntity = level.getBlockEntity(pos)
+
+			if (blockEntity is EnderPorcupineBlockEntity) {
+				val linkPos = blockEntity.linkedPosition
+				if (linkPos != null && level.mayInteract(player, linkPos)) {
+					val stateThere = level.getBlockState(linkPos)
+					val broken = stateThere.block.playerWillDestroy(level, linkPos, stateThere, player)
+				}
+			}
+		}
+
+		return super.playerWillDestroy(level, pos, state, player)
 	}
 
 }
