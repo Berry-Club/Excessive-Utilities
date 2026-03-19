50b99d080ac23d901c4b923d695189c3d39e1c13
aaron <aaronhowser1@gmail.com>
2026-03-19 17:40:00 -0500

    add Unstable Ingot recipe builder class

---
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/recipe/builder/UnstableIngotRecipeBuilder.kt
@@ -0,0 +1,75 @@
+package dev.aaronhowser.mods.excessive_utilities.datagen.recipe.builder
+
+import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import dev.aaronhowser.mods.excessive_utilities.recipe.UnstableIngotRecipe
+import net.minecraft.advancements.AdvancementRequirements
+import net.minecraft.advancements.AdvancementRewards
+import net.minecraft.advancements.Criterion
+import net.minecraft.advancements.critereon.RecipeUnlockedTrigger
+import net.minecraft.data.recipes.RecipeBuilder
+import net.minecraft.data.recipes.RecipeOutput
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.Ingredient
+import net.minecraft.world.item.crafting.ShapedRecipePattern
+
+class UnstableIngotRecipeBuilder(
+	val output: ItemStack
+) : RecipeBuilder {
+
+	private val rows = mutableListOf<String>()
+	private val key = mutableMapOf<Char, Ingredient>()
+	private val criteria = mutableMapOf<String, Criterion<*>>()
+
+	fun define(symbol: Char, ingredient: Ingredient): UnstableIngotRecipeBuilder {
+		key[symbol] = ingredient
+		return this
+	}
+
+	fun pattern(vararg rows: String): UnstableIngotRecipeBuilder {
+		this.rows.addAll(rows)
+
+		val width = rows.first().length
+		if (rows.any { it.length != width }) {
+			throw IllegalArgumentException("All rows must have the same length")
+		}
+
+		return this
+	}
+
+	override fun unlockedBy(name: String, criterion: Criterion<*>): RecipeBuilder {
+		criteria[name] = criterion
+		return this
+	}
+
+	override fun group(groupName: String?): RecipeBuilder {
+		error("UnstableIngotRecipe does not support recipe groups")
+	}
+
+	override fun getResult(): Item = output.item
+
+	override fun save(recipeOutput: RecipeOutput, id: ResourceLocation) {
+		val idString = StringBuilder()
+
+		idString
+			.append("unstable_ingot/")
+			.append(id.path)
+
+		val id = ExcessiveUtilities.modResource(idString.toString())
+
+		val advancement = recipeOutput.advancement()
+			.addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(id))
+			.rewards(AdvancementRewards.Builder.recipe(id))
+			.requirements(AdvancementRequirements.Strategy.OR)
+
+		for (criterion in criteria) {
+			advancement.addCriterion(criterion.key, criterion.value)
+		}
+
+		val pattern = ShapedRecipePattern.of(key, rows)
+		val recipe = UnstableIngotRecipe(pattern, output)
+		recipeOutput.accept(id, recipe, advancement.build(id.withPrefix("recipes/unstable_ingot/")))
+	}
+
+}
