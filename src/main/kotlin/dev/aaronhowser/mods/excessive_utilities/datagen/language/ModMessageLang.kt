7519010c7a416c78b99351695095ef922a84e75a
aaron <aaronhowser1@gmail.com>
2026-03-21 07:09:34 -0500

    tell them what the apple does

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/language/ModMessageLang.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/language/ModMessageLang.kt
@@ -3,11 +3,13 @@ package dev.aaronhowser.mods.excessive_utilities.datagen.language
 object ModMessageLang {
 
 	const val DOOM_EFFECT_TIME = "message.excessive_utilities.doom_effect_time"
+	const val EAT_MAGICAL_APPLE = "message.excessive_utilities.eat_magical_apple"
 	const val DOOM_DEATH = "death.attack.eu_doom"
 
 	fun add(provider: ModLanguageProvider) {
 		provider.apply {
 			add(DOOM_EFFECT_TIME, "The Spectre of Death will arrive in %d seconds.")
+			add(EAT_MAGICAL_APPLE, "You feel your luck changing.")
 			add(DOOM_DEATH, "%s met their doom.")
 		}
 	}
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/MagicalAppleItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/item/MagicalAppleItem.kt
@@ -1,5 +1,8 @@
 package dev.aaronhowser.mods.excessive_utilities.item
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.tell
+import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModLanguageProvider.Companion.toComponent
+import dev.aaronhowser.mods.excessive_utilities.datagen.language.ModMessageLang
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.food.FoodProperties
@@ -12,6 +15,7 @@ class MagicalAppleItem(properties: Properties) : Item(properties) {
 	override fun finishUsingItem(stack: ItemStack, level: Level, livingEntity: LivingEntity): ItemStack {
 		if (livingEntity is Player) {
 			livingEntity.enchantmentSeed = level.random.nextInt()
+			livingEntity.tell(ModMessageLang.EAT_MAGICAL_APPLE.toComponent())
 		}
 
 		return super.finishUsingItem(stack, level, livingEntity)
