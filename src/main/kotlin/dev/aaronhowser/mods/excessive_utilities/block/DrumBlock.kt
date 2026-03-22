2cd0cbbde7fd999efd56523b4960e7f5b99fe239
aaron <aaronhowser1@gmail.com>
2026-03-21 21:13:13 -0500

    shape

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/DrumBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/DrumBlock.kt
@@ -6,6 +6,7 @@ import net.minecraft.world.InteractionHand
 import net.minecraft.world.ItemInteractionResult
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
@@ -13,6 +14,8 @@ import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.VoxelShape
 import net.neoforged.neoforge.fluids.FluidUtil
 
 class DrumBlock : Block(Properties.ofFullCopy(Blocks.IRON_BLOCK)), EntityBlock {
@@ -32,6 +35,14 @@ class DrumBlock : Block(Properties.ofFullCopy(Blocks.IRON_BLOCK)), EntityBlock {
 	): ItemInteractionResult {
 		val fluidTransferred = FluidUtil.interactWithFluidHandler(player, hand, level, pos, hitResult.direction)
 		return if (fluidTransferred) ItemInteractionResult.sidedSuccess(level.isClientSide) else ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION
+	}
+
+	override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
+		return SHAPE
+	}
+
+	companion object {
+		val SHAPE: VoxelShape = box(2.0, 0.0, 2.0, 14.0, 16.0, 14.0)
 	}
 
 }
