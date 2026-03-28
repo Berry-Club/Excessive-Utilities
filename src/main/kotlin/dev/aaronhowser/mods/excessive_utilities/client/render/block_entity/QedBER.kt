7e4282ddbe5b1e22ffb8a569cc1bcde61fb2980d
aaron <aaronhowser1@gmail.com>
2026-03-28 10:57:49 -0500

    feat: update box rendering to use RGBA color values for enhanced visual fidelity

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/block_entity/QedBER.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/client/render/block_entity/QedBER.kt
@@ -2,6 +2,7 @@ package dev.aaronhowser.mods.excessive_utilities.client.render.block_entity
 
 import com.mojang.blaze3d.vertex.PoseStack
 import dev.aaronhowser.mods.aaron.client.AaronClientUtil
+import dev.aaronhowser.mods.aaron.misc.RGBA
 import dev.aaronhowser.mods.excessive_utilities.block_entity.QedBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.client.render.RenderUtil
 import dev.aaronhowser.mods.excessive_utilities.client.render.WandRenderer
@@ -38,12 +39,20 @@ class QedBER(
 		val maxY = radius + 1
 		val maxZ = radius + 1
 
+		val color = RGBA.fromInt(0x12BCA3FF)
+		val (rInt, gInt, bInt, aInt) = color
+
+		val r = rInt / 255f
+		val g = gInt / 255f
+		val b = bInt / 255f
+		val a = aInt / 255f
+
 		RenderUtil.box(
 			poseStack,
 			linesConsumer,
 			minX, minY, minZ,
 			maxX, maxY, maxZ,
-			1f, 1f, 1f, 1f
+			r, g, b, a
 		)
 
 	}
