d3b3b219add083068a3b3f81dc8ebbffd9524ba3
aaron <aaronhowser1@gmail.com>
2026-06-08 03:15:06 -0500

    Add enchantment power display to Enchanter JEI category

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/compatibility/jei/category/EnchanterJeiCategory.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/compatibility/jei/category/EnchanterJeiCategory.kt
@@ -10,12 +10,16 @@ import mezz.jei.api.helpers.IGuiHelper
 import mezz.jei.api.recipe.IFocusGroup
 import mezz.jei.api.recipe.RecipeType
 import mezz.jei.api.recipe.category.AbstractRecipeCategory
+import net.minecraft.client.Minecraft
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Items
 import net.minecraft.world.item.crafting.RecipeHolder
 
 class EnchanterJeiCategory(
 	recipeType: RecipeType<RecipeHolder<EnchanterRecipe>>,
-	guiHelper: IGuiHelper
+	val guiHelper: IGuiHelper
 ) : AbstractRecipeCategory<RecipeHolder<EnchanterRecipe>>(
 	recipeType,
 	ModBlocks.ENCHANTER.get().name,
@@ -27,9 +31,9 @@ class EnchanterJeiCategory(
 	override fun setRecipe(builder: IRecipeLayoutBuilder, recipe: RecipeHolder<EnchanterRecipe>, focuses: IFocusGroup) {
 		val recipe = recipe.value()
 
-		val leftInputSlot = builder.addInputSlot(9, 9)
-		val rightInputSlot = builder.addInputSlot(33, 9)
-		val outputSlot = builder.addOutputSlot(90, 9)
+		val leftInputSlot = builder.addInputSlot(9, 19)
+		val rightInputSlot = builder.addInputSlot(33, 19)
+		val outputSlot = builder.addOutputSlot(90, 19)
 
 		val inputs = recipe.leftIngredient.items.map { it.copyWithCount(recipe.leftIngredient.count()) }
 		leftInputSlot
@@ -49,20 +53,34 @@ class EnchanterJeiCategory(
 	override fun createRecipeExtras(builder: IRecipeExtrasBuilder, recipe: RecipeHolder<EnchanterRecipe>, focuses: IFocusGroup) {
 		val recipe = recipe.value()
 
-		builder.addRecipeArrow().setPosition(55, 9)
+		builder.addRecipeArrow().setPosition(55, 19)
 
 		val formattedFePerTick = "%,d".format(recipe.fePerTick)
 		val formattedTicks = "%,d".format(recipe.ticks)
+		val formattedEnchantPower = recipe.enchantingPower.toString()
 
 		builder.addText(
 			ModMenuLang.FE_PER_TICK.toComponent(formattedFePerTick),
 			80, 12
-		).setPosition(9, 32).setColor(0xFF808080.toInt())
+		).setPosition(9, 42).setColor(0xFF808080.toInt())
 
 		builder.addText(
 			ModMenuLang.TICKS.toComponent(formattedTicks),
 			80, 12
-		).setPosition(65, 32).setColor(0xFF808080.toInt())
+		).setPosition(65, 42).setColor(0xFF808080.toInt())
+
+		val enchantPowerText = Component.literal("${formattedEnchantPower}x ")
+		val enchantWidth = Minecraft.getInstance().font.width(enchantPowerText)
+
+		builder.addText(
+			enchantPowerText,
+			enchantWidth + 2, 12
+		).setPosition(5, 4).setColor(0xFF808080.toInt())
+
+		builder.addDrawable(
+			guiHelper.createDrawableItemLike(Items.BOOKSHELF),
+			5 + enchantWidth, 0
+		)
 	}
 
 	override fun getRegistryName(recipe: RecipeHolder<EnchanterRecipe>): ResourceLocation = recipe.id
