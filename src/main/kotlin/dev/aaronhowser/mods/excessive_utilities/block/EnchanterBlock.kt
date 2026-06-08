aeb60c6a65816cf4dbf39118c4524fdb9b772150
aaron <aaronhowser1@gmail.com>
2026-06-08 02:54:23 -0500

    animation

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnchanterBlock.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/block/EnchanterBlock.kt
@@ -1,12 +1,18 @@
 package dev.aaronhowser.mods.excessive_utilities.block
 
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.nextRange
 import dev.aaronhowser.mods.excessive_utilities.block.base.GpDrainBlock
 import dev.aaronhowser.mods.excessive_utilities.block_entity.base.GpDrainBlockEntity
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlockEntityTypes
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.core.particles.ParticleTypes
+import net.minecraft.util.RandomSource
 import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.block.EnchantingTableBlock
 import net.minecraft.world.level.block.entity.BlockEntityType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
@@ -35,6 +41,29 @@ class EnchanterBlock : GpDrainBlock(Properties.ofFullCopy(Blocks.IRON_BLOCK)) {
 
 	override fun getBlockEntityType(): BlockEntityType<out GpDrainBlockEntity> {
 		return ModBlockEntityTypes.ENCHANTER.get()
+	}
+
+	override fun animateTick(state: BlockState, level: Level, pos: BlockPos, random: RandomSource) {
+		if (!state.getValue(ENABLED)) return
+
+		for (offset in EnchantingTableBlock.BOOKSHELF_OFFSETS) {
+			if (random.nextInt(16) != 0
+				|| !EnchantingTableBlock.isValidBookShelf(level, pos, offset)
+			) continue
+
+			val targetX = pos.x.toDouble() + random.nextRange(0.2, 0.8)
+			val targetZ = pos.z.toDouble() + random.nextRange(0.2, 0.8)
+
+			level.addParticle(
+				ParticleTypes.ENCHANT,
+				targetX,
+				pos.y.toDouble() + 2.0,
+				targetZ,
+				(offset.x.toFloat() + random.nextFloat()).toDouble() - 0.5,
+				(offset.y.toFloat() - random.nextFloat() - 1.0f).toDouble(),
+				(offset.z.toFloat() + random.nextFloat()).toDouble() - 0.5
+			)
+		}
 	}
 
 	companion object {
