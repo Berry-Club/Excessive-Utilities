86b5635007d7d93a0d5141a8d66d73c7933334f4
aaron <aaronhowser1@gmail.com>
2026-06-10 07:03:09 -0500

    y

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/datapack/worldgen/ModPlacedFeatures.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/datapack/worldgen/ModPlacedFeatures.kt
@@ -4,7 +4,10 @@ import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.worldgen.BootstrapContext
 import net.minecraft.resources.ResourceKey
+import net.minecraft.world.level.levelgen.VerticalAnchor
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider
 import net.minecraft.world.level.levelgen.placement.BiomeFilter
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement
 import net.minecraft.world.level.levelgen.placement.InSquarePlacement
 import net.minecraft.world.level.levelgen.placement.PlacedFeature
 import net.minecraft.world.level.levelgen.placement.RarityFilter
@@ -24,6 +27,10 @@ object ModPlacedFeatures {
 				listOf(
 					BiomeFilter.biome(),
 					InSquarePlacement.spread(),
+					HeightRangePlacement.uniform(
+						VerticalAnchor.absolute(71),
+						VerticalAnchor.absolute(71),
+					),
 					RarityFilter.onAverageOnceEvery(100)
 				)
 			)
