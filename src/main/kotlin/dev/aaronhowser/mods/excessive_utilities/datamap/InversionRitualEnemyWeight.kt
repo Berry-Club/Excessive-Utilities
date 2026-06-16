30f8cad68a48c8eeb3a63372b0b2b43d3f5aada5
aaron <aaronhowser1@gmail.com>
2026-06-15 19:53:07 -0500

    hmmm

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datamap/InversionRitualEnemyWeight.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datamap/InversionRitualEnemyWeight.kt
@@ -2,7 +2,9 @@ package dev.aaronhowser.mods.excessive_utilities.datamap
 
 import com.mojang.serialization.Codec
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
+import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
+import net.minecraft.util.RandomSource
 import net.minecraft.world.entity.EntityType
 import net.neoforged.neoforge.registries.datamaps.DataMapType
 
@@ -24,6 +26,35 @@ class InversionRitualEnemyWeight(
 				.synced(CODEC, true)
 				.build()
 
+		fun getRandomType(random: RandomSource): EntityType<*>? {
+			val weights = getWeightedTypes()
+
+			val totalWeight = weights.values.sum()
+			var roll = random.nextDouble() * totalWeight
+
+			for ((type, weight) in weights) {
+				roll -= weight
+				if (roll < 0.0) {
+					return type
+				}
+			}
+
+			return null
+		}
+
+		fun getWeightedTypes(): Map<EntityType<*>, Double> {
+			val weights = mutableMapOf<EntityType<*>, Double>()
+
+			val registry = BuiltInRegistries.ENTITY_TYPE
+			for (type in registry) {
+				val weight = type.builtInRegistryHolder().getData(DATA_MAP)
+				if (weight != null) {
+					weights[type] = weight.weight
+				}
+			}
+
+			return weights
+		}
 	}
 
 }
