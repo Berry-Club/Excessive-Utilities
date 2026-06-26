8af7d6606708e4179c0fe245c490f3099349d92c
aaron <aaronhowser1@gmail.com>
2026-06-26 05:08:48 -0500

    use the aaron dsl

--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/loot/ModBlockLootTablesSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/loot/ModBlockLootTablesSubProvider.kt
@@ -1,5 +1,6 @@
 package dev.aaronhowser.mods.excessive_utilities.datagen.loot
 
+import dev.aaronhowser.mods.aaron.datagen.AaronLootTableDsl
 import dev.aaronhowser.mods.excessive_utilities.registry.ModBlocks
 import dev.aaronhowser.mods.excessive_utilities.registry.ModDataComponents
 import dev.aaronhowser.mods.excessive_utilities.registry.ModItems
@@ -22,14 +23,11 @@ import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.CropBlock
 import net.minecraft.world.level.storage.loot.IntRange
 import net.minecraft.world.level.storage.loot.LootPool
-import net.minecraft.world.level.storage.loot.LootTable
-import net.minecraft.world.level.storage.loot.entries.LootItem
 import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction
 import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition
 import net.minecraft.world.level.storage.loot.predicates.LocationCheck
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition
 import net.minecraft.world.level.storage.loot.predicates.TimeCheck
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue
 import net.neoforged.neoforge.common.Tags
 import net.neoforged.neoforge.registries.DeferredHolder
 
@@ -73,132 +71,124 @@ class ModBlockLootTablesSubProvider(
 
 		add(
 			ModBlocks.MOON_STONE_ORE.get(),
-			LootTable.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(ModItems.MOON_STONE))
-						.`when`(TimeCheck.time(IntRange.range(13000, 23000)))
-				)
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(Items.COBBLESTONE))
-				)
+			AaronLootTableDsl.table {
+				pool {
+					rolls(1f)
+					item(ModItems.MOON_STONE)
+					condition(TimeCheck.time(IntRange.range(13000, 23000)))
+				}
+				pool {
+					rolls(1f)
+					item(Items.COBBLESTONE)
+				}
+			}
 		)
 
 		add(
 			ModBlocks.DEEPSLATE_MOON_STONE_ORE.get(),
-			LootTable.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(ModItems.MOON_STONE))
-						.`when`(TimeCheck.time(IntRange.range(13000, 23000)))
-				)
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(Items.COBBLED_DEEPSLATE))
-				)
+			AaronLootTableDsl.table {
+				pool {
+					rolls(1f)
+					item(ModItems.MOON_STONE)
+					condition(TimeCheck.time(IntRange.range(13000, 23000)))
+				}
+				pool {
+					rolls(1f)
+					item(Items.COBBLED_DEEPSLATE)
+				}
+			}
 		)
 
 		add(ModBlocks.CURSED_EARTH.get()) { block -> createSingleItemTableWithSilkTouch(block, Blocks.DIRT) }
 
 		add(
 			ModBlocks.RESTURBED_MOB_SPAWNER.get(),
-			LootTable.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(
-							LootItem.lootTableItem(ModBlocks.RESTURBED_MOB_SPAWNER.asItem())
-								.apply(
-									CopyComponentsFunction.copyComponents(
-										CopyComponentsFunction.Source.BLOCK_ENTITY
-									)
-										.include(DataComponents.BLOCK_ENTITY_DATA)
-								)
+			AaronLootTableDsl.table {
+				pool {
+					rolls(1f)
+					item(ModBlocks.RESTURBED_MOB_SPAWNER.asItem()) {
+						apply(
+							CopyComponentsFunction.copyComponents(
+								CopyComponentsFunction.Source.BLOCK_ENTITY
+							)
+								.include(DataComponents.BLOCK_ENTITY_DATA)
 						)
-				)
+					}
+				}
+			}
 		)
 
 		add(
 			ModBlocks.MAGICAL_SNOW_GLOBE.get(),
-			LootTable.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(
-							LootItem.lootTableItem(ModBlocks.MAGICAL_SNOW_GLOBE.asItem())
-								.apply(
-									CopyComponentsFunction.copyComponents(
-										CopyComponentsFunction.Source.BLOCK_ENTITY
-									)
-										.include(ModDataComponents.MAGICAL_SNOW_GLOBE_PROGRESS.get())
-								)
+			AaronLootTableDsl.table {
+				pool {
+					rolls(1f)
+					item(ModBlocks.MAGICAL_SNOW_GLOBE.asItem()) {
+						apply(
+							CopyComponentsFunction.copyComponents(
+								CopyComponentsFunction.Source.BLOCK_ENTITY
+							)
+								.include(ModDataComponents.MAGICAL_SNOW_GLOBE_PROGRESS.get())
 						)
-				)
+					}
+				}
+			}
 		)
 
 		for (drum in drums) {
 			add(
 				drum,
-				LootTable.lootTable()
-					.withPool(
-						LootPool.lootPool()
-							.setRolls(ConstantValue.exactly(1f))
-							.add(
-								LootItem.lootTableItem(drum)
-									.apply(
-										CopyComponentsFunction.copyComponents(
-											CopyComponentsFunction.Source.BLOCK_ENTITY
-										)
-											.include(ModDataComponents.TANK.get())
-									)
+				AaronLootTableDsl.table {
+					pool {
+						rolls(1f)
+						item(drum) {
+							apply(
+								CopyComponentsFunction.copyComponents(
+									CopyComponentsFunction.Source.BLOCK_ENTITY
+								)
+									.include(ModDataComponents.TANK.get())
 							)
-					)
+						}
+					}
+				}
 			)
 		}
 
 		add(
 			ModBlocks.ENDER_LILY.get(),
-			LootTable.lootTable()
+			AaronLootTableDsl.table {
 
 				// Always drop the seeds
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(ModItems.ENDER_LILY))
-				)
+				pool {
+					rolls(1f)
+					item(ModItems.ENDER_LILY)
+				}
 
 				// Drop one Ender Pearl if it's fully grown
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(Items.ENDER_PEARL))
-						.`when`(
+				pool {
+					rolls(1f)
+					item(Items.ENDER_PEARL)
+					condition(
 							LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.ENDER_LILY.get())
 								.setProperties(
 									StatePropertiesPredicate.Builder.properties()
 										.hasProperty(CropBlock.AGE, 7)
 								)
-						)
-				)
+					)
+				}
 
 				// Drop another if on End Stone and fully grown
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(Items.ENDER_PEARL))
-						.`when`(
+				pool {
+					rolls(1f)
+					item(Items.ENDER_PEARL)
+					condition(
 							LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.ENDER_LILY.get())
 								.setProperties(
 									StatePropertiesPredicate.Builder.properties()
 										.hasProperty(CropBlock.AGE, 7)
 								)
-						)
-						.`when`(
+					)
+					condition(
 							LocationCheck.checkLocation(
 								LocationPredicate.Builder.location()
 									.setBlock(
@@ -207,50 +197,50 @@ class ModBlockLootTablesSubProvider(
 									),
 								BlockPos(0, -1, 0)
 							)
-						)
-				)
+					)
+				}
 
 				// Drop another for each level of Fortune, if fully grown
-				.withPool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 1))
-				.withPool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 2))
-				.withPool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 3))
-				.withPool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 4))
-				.withPool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 5))
+				pool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 1))
+				pool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 2))
+				pool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 3))
+				pool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 4))
+				pool(createFortuneBonusPool(ModBlocks.ENDER_LILY.get(), Items.ENDER_PEARL, fortune, 5))
 
+			}
 		)
 
 		add(
 			ModBlocks.RED_ORCHID.get(),
-			LootTable.lootTable()
+			AaronLootTableDsl.table {
 
 				// Always drop the seeds
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(ModItems.RED_ORCHID))
-				)
+				pool {
+					rolls(1f)
+					item(ModItems.RED_ORCHID)
+				}
 
 				// Drop one Redstone Dust if it's fully grown
-				.withPool(
-					LootPool.lootPool()
-						.setRolls(ConstantValue.exactly(1f))
-						.add(LootItem.lootTableItem(Items.REDSTONE))
-						.`when`(
+				pool {
+					rolls(1f)
+					item(Items.REDSTONE)
+					condition(
 							LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RED_ORCHID.get())
 								.setProperties(
 									StatePropertiesPredicate.Builder.properties()
 										.hasProperty(CropBlock.AGE, 7)
 								)
-						)
-				)
+					)
+				}
 
 				// Drop another for each level of Fortune, if fully grown
-				.withPool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 1))
-				.withPool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 2))
-				.withPool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 3))
-				.withPool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 4))
-				.withPool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 5))
+				pool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 1))
+				pool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 2))
+				pool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 3))
+				pool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 4))
+				pool(createFortuneBonusPool(ModBlocks.RED_ORCHID.get(), Items.REDSTONE, fortune, 5))
 
+			}
 		)
 
 	}
@@ -265,19 +255,18 @@ class ModBlockLootTablesSubProvider(
 			if (index < minFortuneLevel) 0f else 1f
 		}
 
-		return LootPool.lootPool()
-			.setRolls(ConstantValue.exactly(1f))
-			.add(LootItem.lootTableItem(drop))
-			.`when`(
+		return AaronLootTableDsl.pool {
+			rolls(1f)
+			item(drop)
+			condition(
 				LootItemBlockStatePropertyCondition.hasBlockStateProperties(crop)
 					.setProperties(
 						StatePropertiesPredicate.Builder.properties()
 							.hasProperty(CropBlock.AGE, 7)
 					)
 			)
-			.`when`(
-				BonusLevelTableCondition.bonusLevelFlatChance(fortune, *chances)
-			)
+			condition(BonusLevelTableCondition.bonusLevelFlatChance(fortune, *chances))
+		}
 	}
 
 }
--- src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/loot/OtherLootTableSubProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/excessive_utilities/datagen/loot/OtherLootTableSubProvider.kt
@@ -1,6 +1,6 @@
 package dev.aaronhowser.mods.excessive_utilities.datagen.loot
 
-import dev.aaronhowser.mods.aaron.datagen.LootItemStack
+import dev.aaronhowser.mods.aaron.datagen.AaronLootTableDsl
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.toComponent
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.withComponent
 import dev.aaronhowser.mods.excessive_utilities.ExcessiveUtilities
@@ -11,13 +11,8 @@ import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
 import net.minecraft.data.loot.LootTableSubProvider
 import net.minecraft.resources.ResourceKey
-import net.minecraft.util.Mth
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.component.ItemLore
-import net.minecraft.world.level.storage.loot.LootPool
 import net.minecraft.world.level.storage.loot.LootTable
-import net.minecraft.world.level.storage.loot.entries.EmptyLootItem
-import net.minecraft.world.level.storage.loot.entries.LootItem
 import java.util.function.BiConsumer
 
 class OtherLootTableSubProvider(
@@ -28,21 +23,17 @@ class OtherLootTableSubProvider(
 
 		output.accept(
 			DROP_OF_EVIL,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DROP_OF_EVIL.get(), .1f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DROP_OF_EVIL.get(), .1f))
+			}
 		)
 
 
 		output.accept(
 			RESONATING_REDSTONE_CRYSTAL,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.RESONATING_REDSTONE_CRYSTAL.get(), .3f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.RESONATING_REDSTONE_CRYSTAL.get(), .3f))
+			}
 		)
 
 		val forgottenSoul = ModItems.SOUL_FRAGMENT.withComponent(
@@ -54,99 +45,78 @@ class OtherLootTableSubProvider(
 
 		output.accept(
 			SOUL_FRAGMENT,
-			LootTable
-				.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.add(EmptyLootItem.emptyItem().setWeight(43046721))
-						.add(LootItemStack.lootTableStack(forgottenSoul).setWeight(1))
-				)
+			AaronLootTableDsl.table {
+				pool {
+					empty(43_046_721)
+					stack(forgottenSoul, 1)
+				}
+			}
 		)
 
 		output.accept(
 			SOUL_FRAGMENT_KIKOKU,
-			LootTable
-				.lootTable()
-				.withPool(
-					LootPool.lootPool()
-						.add(EmptyLootItem.emptyItem().setWeight(43046721))
-						.add(LootItemStack.lootTableStack(forgottenSoul).setWeight(10))
-				)
+			AaronLootTableDsl.table {
+				pool {
+					empty(43_046_721)
+					stack(forgottenSoul, 10)
+				}
+			}
 		)
 
 		output.accept(
 			DIVISION_SIGIL_STRONGHOLD_CORRIDOR,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DIVISION_SIGIL.get(), 0.011f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DIVISION_SIGIL.get(), 0.011f))
+			}
 		)
 
 		output.accept(
 			DIVISION_SIGIL_STRONGHOLD_CROSSING,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DIVISION_SIGIL.get(), 0.013f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DIVISION_SIGIL.get(), 0.013f))
+			}
 		)
 
 		output.accept(
 			DIVISION_SIGIL_MINESHAFT,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DIVISION_SIGIL.get(), 0.045f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DIVISION_SIGIL.get(), 0.045f))
+			}
 		)
 
 		output.accept(
 			DIVISION_SIGIL_DUNGEON,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DIVISION_SIGIL.get(), 0.01f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DIVISION_SIGIL.get(), 0.01f))
+			}
 		)
 
 		output.accept(
 			DIVISION_SIGIL_DESERT_TEMPLE,
-			LootTable
-				.lootTable()
-				.withPool(
-					singleItemPool(ModItems.DIVISION_SIGIL.get(), 0.021f)
-				)
+			AaronLootTableDsl.table {
+				pool(AaronLootTableDsl.singleItemChancePool(ModItems.DIVISION_SIGIL.get(), 0.021f))
+			}
 		)
 	}
 
 	companion object {
-		private fun createPoolRk(name: String): ResourceKey<LootTable> {
+		private fun key(name: String): ResourceKey<LootTable> {
 			return ResourceKey.create(
 				Registries.LOOT_TABLE,
 				ExcessiveUtilities.modResource(name)
 			)
 		}
 
-		private fun singleItemPool(item: Item, chance: Float): LootPool.Builder {
-			val itemWeight = Mth.ceil(chance * 10_000)
-			val emptyWeight = 10_000 - itemWeight
+		val DROP_OF_EVIL = key("entity/drop_of_evil")
+		val RESONATING_REDSTONE_CRYSTAL = key("block/resonating_redstone_crystal")
+		val SOUL_FRAGMENT = key("entity/soul_fragment")
+		val SOUL_FRAGMENT_KIKOKU = key("entity/soul_fragment_kikoku")
 
-			return LootPool.lootPool()
-				.add(EmptyLootItem.emptyItem().setWeight(emptyWeight))
-				.add(LootItem.lootTableItem(item).setWeight(itemWeight))
-		}
-
-		val DROP_OF_EVIL = createPoolRk("entity/drop_of_evil")
-		val RESONATING_REDSTONE_CRYSTAL = createPoolRk("block/resonating_redstone_crystal")
-		val SOUL_FRAGMENT = createPoolRk("entity/soul_fragment")
-		val SOUL_FRAGMENT_KIKOKU = createPoolRk("entity/soul_fragment_kikoku")
-
-		val DIVISION_SIGIL_STRONGHOLD_CORRIDOR = createPoolRk("structure/division_sigils/stronghold_corridor")
-		val DIVISION_SIGIL_STRONGHOLD_CROSSING = createPoolRk("structure/division_sigils/stronghold_crossing")
-		val DIVISION_SIGIL_MINESHAFT = createPoolRk("structure/division_sigils/mineshaft")
-		val DIVISION_SIGIL_DUNGEON = createPoolRk("structure/division_sigils/dungeon")
-		val DIVISION_SIGIL_DESERT_TEMPLE = createPoolRk("structure/division_sigils/desert_temple")
+		val DIVISION_SIGIL_STRONGHOLD_CORRIDOR = key("structure/division_sigils/stronghold_corridor")
+		val DIVISION_SIGIL_STRONGHOLD_CROSSING = key("structure/division_sigils/stronghold_crossing")
+		val DIVISION_SIGIL_MINESHAFT = key("structure/division_sigils/mineshaft")
+		val DIVISION_SIGIL_DUNGEON = key("structure/division_sigils/dungeon")
+		val DIVISION_SIGIL_DESERT_TEMPLE = key("structure/division_sigils/desert_temple")
 	}
 
 }
